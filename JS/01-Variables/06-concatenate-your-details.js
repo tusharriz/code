060332662a82763b3a4c11fe46273790a37be514
Tushar Riz <tushariizzz@gmail.com>
2025-01-05 18:21:44 +0530

    coding exercise

---
+++ JS/01-Variables/06-concatenate-your-details.js
@@ -0,0 +1,9 @@
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Concatenate and console them in a new string to get the output like this : ("My name is"+ name +"and my age is"+ age +".")
+
+let firstName = "Steve";
+let lastName = "Jobs";
+let age = 19;
+
+console.log("My name is" + " " + firstName + " " + lastName + " " +"and my age is"+ " " + age +".");
