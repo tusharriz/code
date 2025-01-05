b4b57d1b9aeee4227cb29dbcc32865f4c7a4fd6f
Tushar Riz <tushariizzz@gmail.com>
2025-01-05 18:24:10 +0530

    coding exercise

---
+++ JS/01-Variables/07-print-in-next-line.js
@@ -0,0 +1,5 @@
+// print JS, DSA and WEB-DEV in the next line but use only one "console.log" statement.
+
+console.log("JS" + "\n" + "DSA" + "\n" + "WEB-DEV");
+// or
+console.log("JS\nDSA\nWEB-DEV");
