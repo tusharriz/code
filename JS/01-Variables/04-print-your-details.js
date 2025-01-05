50bd959f31a4748340a43469abb6ffcf38f55a16
Tushar Riz <tushariizzz@gmail.com>
2025-01-05 18:19:32 +0530

    coding exercise

---
+++ JS/01-Variables/04-print-your-details.js
@@ -0,0 +1,17 @@
+// Define a variable called firstName and lastName with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let firstName = "Steve";
+let lastName = "Jobs";
+console.log(firstName + " " + lastName); // Steve Jobs
+
+let fatherName = "Danial Jobs";
+console.log(fatherName); // Danial Jobs
+
+let motherName = "Lisa Jobs";
+console.log(motherName); //Lisa Jobs
+
