9e6008ab074f5e2c3f12d1bbf572f69d7b0a7c9c
Tushar Riz <tushariizzz@gmail.com>
2025-01-05 18:13:46 +0530

    coding exercise

---
+++ JS/01-Variables/01-declare-a-variable.js
@@ -0,0 +1,10 @@
+// declare a variable "x" with the value of "10" and print the value.
+
+let x = 10;
+console.log(x);
+
+
+// declare a variable "y" with the value of "20" and print the value.
+
+let y = 20;
+console.log(y);
