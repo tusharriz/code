aa23187c89871d80aa7c5a2bd62d0c8d0a34c481
Tushar Riz <tushariizzz@gmail.com>
2025-01-06 19:23:08 +0530

    coding exercise

---
+++ JS/02-Mathematical-Operator/01-print-the-exact-output.js
@@ -0,0 +1,18 @@
+// find sum, difference, product and division;
+// output like this:-
+
+// Sum is 5
+// Difference is 1
+// Product is 6
+// Divsion is 1.5
+
+let a = 3;
+let b = 2;
+
+console.log("Sum is", a+b );
+console.log("Difference is", a-b );
+console.log("Product is", a*b );
+console.log("Divsion is", a/b );
+
+
+
