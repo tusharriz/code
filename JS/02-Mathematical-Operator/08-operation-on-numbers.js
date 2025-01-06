8111742acad7f5f8ef63a34607882f10e335f302
Tushar Riz <48861473+tusharriz@users.noreply.github.com>
2025-01-06 23:26:59 +0530

    coding exercise

--- JS/02-Mathematical-Operator/08-operation-on-numbers.js
+++ JS/02-Mathematical-Operator/08-operation-on-numbers.js
@@ -1,9 +1,7 @@
 `You are given a number, stored in a variable with the name number . Perform the following operations on the value stored in the number
-
 1. Multiply the value by 3
 2. Add 7 after it
 3. Subtract 10 from it
-
 After performing all the above operations, print the updated value`
 
 let number = 4;
