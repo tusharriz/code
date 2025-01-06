1c43101f27ee6ea1dd92f53984d742233e32ba1a
Tushar Riz <tushariizzz@gmail.com>
2025-01-06 22:01:29 +0530

    coding exercise

---
+++ JS/02-Mathematical-Operator/07-Multiply-by-50.js
@@ -0,0 +1,11 @@
+// Multiply by 50
+
+// You are given a number stored in a variable, with the name number
+
+// Multiply the number stored in the variable number by 50, and print the result obtained
+
+let number = 2;
+
+let result = number * 50;
+
+console.log(result); // 100
