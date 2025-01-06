ea198c3d7d985fca88991666b5d51a25d72065d3
Tushar Riz <tushariizzz@gmail.com>
2025-01-06 22:13:58 +0530

    coding exercise

---
+++ JS/02-Mathematical-Operator/11-double-all.js
@@ -0,0 +1,15 @@
+// You are given five numbers, stored in the variables with the following names
+// one, two, three, four, five
+// All the values are doubled, that is, they are multiplied by 2
+
+// Find the sum of the values stored in one, two, three, four, five , after the above operation are performed
+
+let one = 1; two = 2; three = 3; four = 4; five = 5;
+
+one = one * 2;
+two = two * 2;
+three = three * 2;
+four = four * 2;
+five = five * 2;
+
+console.log( one + two + three + four + five ); // 30
