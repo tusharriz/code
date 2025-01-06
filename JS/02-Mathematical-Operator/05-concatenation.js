54b1e9b492f6fb586215c3f5dffa4e81291a2fef
Tushar Riz <tushariizzz@gmail.com>
2025-01-06 19:40:09 +0530

    coding exercise

---
+++ JS/02-Mathematical-Operator/05-concatenation.js
@@ -0,0 +1,24 @@
+//add firstname and lastname;
+
+let firstName = "Siya"; //string
+
+let lastName = "Ram"; //string
+
+console.log(firstName + " " + lastName); //Siya Ram
+
+// Joined the first name and last name, This is called concatenation.
+
+let a = 10; //number
+let b = "baman"; //string
+
+console.log(a+ " " + b); //10 baman
+
+let c = 15; //number
+console.log(a+c); //25
+
+//Concatenation :-
+
+//1. string + string => string (concatenated)
+//2. number + string => string (concatenated)
+//3. number + number => number (addition)
+
