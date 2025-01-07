baacc0e8f132923aa82b0a630909acc4428d05e4
Tushar Riz <tushariizzz@gmail.com>
2025-01-07 14:20:09 +0530

    coding exercise

---
+++ JS/03-Relation-Operator/0.1-check-ages.js
@@ -0,0 +1,7 @@
+`Compare the ages of two people, Ram and Mohan, to determine if Ram is older.`
+
+let ageOfRam = 26;
+let ageOfMohan = 36;
+
+let isRamOlder = ageOfRam > ageOfMohan;
+console.log("Is Ram older than Mohan =", isRamOlder); //false
