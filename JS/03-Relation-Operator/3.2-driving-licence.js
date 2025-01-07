9b6e82ec1eddb7a67e3d8e1bcf23117a21fad041
Tushar Riz <tushariizzz@gmail.com>
2025-01-07 16:42:12 +0530

    coding exercise

---
+++ JS/03-Relation-Operator/3.2-driving-licence.js
@@ -0,0 +1,7 @@
+`Check if Hari is eligible to drive a vehicle in India based on his age.`
+
+let ageOfHari = 18;
+let legalAge = 18;
+
+let hariCanDrive = ageOfHari >= legalAge ;
+console.log("Can Hari legally drive a vehicle =", hariCanDrive); //true 
