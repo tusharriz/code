1db4b1693578be78ce991271563d64ffde2b8016
Tushar Riz <tushariizzz@gmail.com>
2025-01-07 16:36:59 +0530

    coding exercise

---
+++ JS/03-Relation-Operator/3.1-discount-eligibility.js
@@ -0,0 +1,7 @@
+`Check if customer Baman is eligible for a discount based on his total spending.`
+
+let minimumPurchase = 4000;
+let bamanPurchase = 5000;
+
+let isEligibleForDiscount = bamanPurchase >= minimumPurchase;
+console.log("Is Baman eligible for the discount =", isEligibleForDiscount); // true
