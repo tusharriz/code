1dff5411257be9b4479e1031aba1d99c19b478fa
Tushar Riz <tushariizzz@gmail.com>
2025-01-08 16:19:46 +0530

    coding exercise

---
+++ JS/04-Comparison-Operator/2-guess-the-output.js
@@ -0,0 +1,11 @@
+let a = 5;
+let b = 6;
+let c = "6";
+let d = "cat";
+let e = "-3";
+
+console.log(b == c); // true
+console.log(b == a); // false
+console.log(a >= c); //false
+console.log(c <= e); //false
+console.log(e == d); //false
