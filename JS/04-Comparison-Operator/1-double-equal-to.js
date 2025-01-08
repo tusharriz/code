5a0d0a03a2ce4ee0f8590d694e52eb669aaacfbe
Tushar Riz <tushariizzz@gmail.com>
2025-01-08 16:18:09 +0530

    coding exercise

---
+++ JS/04-Comparison-Operator/1-double-equal-to.js
@@ -0,0 +1,15 @@
+//double equal to (==)
+// true : It gives “true” only when the first value is equal to the second value.
+
+let a = 10;
+let b = 10;
+console.log(a == b); // true
+console.log( "cat" == "cat"); // true
+console.log( 10 == "10" ) // true
+
+// false : It gives “false” only when the first value is not equal to the second value.
+
+let c = 10;
+let d= 20;
+console.log(c == d); // false
+console.log("Cat" == "cat"); // false, JavaScript is case-sensitive, so "Cat" (uppercase C) is not equal to "cat" (lowercase c).
