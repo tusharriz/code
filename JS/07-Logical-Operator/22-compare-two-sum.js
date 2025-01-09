92d377b0880403652691c8bd4672babb24305841
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 19:52:34 +0530

    coding exercise

---
+++ JS/07-Logical-Operator/22-compare-two-sum.js
@@ -0,0 +1,24 @@
+`You are given six numbers, stored in the variables with the following names
+one, two, three, four, five, six
+Find the value of sum1 and sum2 , such that
+sum1 = one + two
+sum2 = four + five
+If both the following two conditions are true, print Yes , else print No
+sum1 > three
+sum2 > six
+
+Sample Input 1 
+1 2 3 4 5 6
+Sample Output 1
+No
+`
+
+let one = 1, two = 2, three = 4, four = 3, five = 5, six = 6;
+let sum1 = one + two;
+let sum2 = four + five;
+
+if(sum1 > three && sum2 > six){
+    console.log("Yes"); 
+}else{
+    console.log("No"); // No
+}
