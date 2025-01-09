041984a0d5ba7731e33ae40dd6009f5001eafbed
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 13:20:47 +0530

    coding evaluation

---
+++ JS/06-Evaluation/5-Power-and-Sum-XI.js
@@ -0,0 +1,16 @@
+`
+You are given two numbers stored in variables with the name N and M respectively
+You have to find the value of sum , such that sum = N^7 + M^7
+For example, consider the value stored in N = 2 , and M = 3
+Therefore, N^7 = 2*2*2*2*2*2*2 = 128 , and M^7 = 3*3*3*3*3*3*3 = 2187
+Therefore, sum = 128 + 2187 = 2315 , which is the required answer
+
+Sample Input 1 
+2 3
+Sample Output 1
+2315`
+
+let n = 2;
+let m = 3;
+let sum = n**7 + m**7;
+console.log(sum); // 2315
