81615b50f4030b1c0fd7eae9359442641dbf1640
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 11:37:57 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/10-hotel-bill-discount.js
@@ -0,0 +1,10 @@
+// Imagine you are programming a system for a hotel that offers a discount on the total bill if the amount exceeds a certain threshold. You want to check if the total bill is eligible for a discount.
+
+let total_bill = 699;
+let discount_start_price = 500;
+
+if (total_bill >= discount_start_price) {
+    console.log("Discount Available"); // Discount Avaliable
+} else {
+    console.log("No discount");
+}
