b861ef8263f6a6682e0acc9daa92afeb7a99b069
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 19:25:09 +0530

    coding exercise

---
+++ JS/07-Logical-Operator/10-print-vowel.js
@@ -0,0 +1,7 @@
+// Given any character, if it is a vowel print "Vowel"
+
+let char = "e";
+
+if(char == "a" || char == "e" || char == "i" || char == "o" || char == "u"){
+    console.log("Vowel");
+}
