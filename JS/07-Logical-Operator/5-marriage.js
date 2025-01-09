4c9b86f673f66505f83f6e64f5a4e07b936a7f59
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 17:52:12 +0530

    coding exercise

---
+++ JS/07-Logical-Operator/5-marriage.js
@@ -0,0 +1,8 @@
+let gender="male";
+let age=23;
+
+if((gender=="male" && age>=24) || (gender=="female" && age>=22)){
+  console.log(gender,"is eligible"); // male is eligible
+} else {
+  console.log(gender,"is Not Eligible");
+}
