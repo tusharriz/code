cf957535fdfc038b62e063a2a5cebf06242413e8
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 11:40:25 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/19-check-multiple-of-3.js
@@ -0,0 +1,16 @@
+// If the number is divisible by 3, print a "multiple of 3".
+
+let number = 6;
+
+if(number%3==0){
+    console.log("multiple of 3"); // multiple of 3
+}
+
+//or we can also do this:
+
+let number1 = 9;
+let remainder = number1 % 3
+
+if(remainder == 0){
+    console.log("multiple of 3"); // multiple of 3
+}
