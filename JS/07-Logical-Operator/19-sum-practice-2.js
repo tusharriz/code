975fdd6f7e23f87e054eba4715b3c657052f37b5
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 19:43:41 +0530

    coding exercise

---
+++ JS/07-Logical-Operator/19-sum-practice-2.js
@@ -0,0 +1,22 @@
+`You are given four numbers, stored in the variables with the following names
+one, two, three, four
+Find the value of sum1 and sum2 , such that
+sum1 = one + two
+sum2 = three + four
+If the sum1 > 10 or sum2 > 15, print Yes , else print No
+
+Sample Input 1 
+1 2 3 4
+Sample Output 1
+No
+`
+
+let one = 1, two = 2, three = 4, four = 3;
+let sum1 = one + two;
+let sum2 = three + four;
+
+if(sum1 > 10 || sum2 > 15){
+    console.log("Yes");  
+}else{
+    console.log("No"); // No
+}
