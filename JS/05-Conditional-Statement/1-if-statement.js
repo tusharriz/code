22f3ea412368388c76c7d74ec81e3d97b6dc12f0
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 11:33:17 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/1-if-statement.js
@@ -0,0 +1,9 @@
+// if Statement : If the outcome of the condition is true only when the compiler will go inside the if statement and perform the task.
+
+if(true){
+  console.log("I am inside the 1st statement"); // true
+}
+
+if(false){
+  console.log("I am inside the 2nd statement");
+}
