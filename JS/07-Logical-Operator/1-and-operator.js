72f56ad3167d19e408b9bc95c849c85754e991e1
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 17:56:12 +0530

    coding exercise

---
+++ JS/07-Logical-Operator/1-and-operator.js
@@ -0,0 +1,8 @@
+// AND Operator (&&)
+
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(true && true); // true
+console.log(false && false); // false
+console.log(true && true && true && false); // false
+
