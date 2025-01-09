60bddddd9b1c69cf05902ea55f0b67051e1fb5b6
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 13:20:31 +0530

    coding evaluation

---
+++ JS/06-Evaluation/4-find-K.js
@@ -0,0 +1,16 @@
+`
+You are given a number K . Find the value of K , after doing the following operations:-
+1. K is multiplied by 7
+2. 10 is added in the new value of K.
+3. K is divided by 2 (print floor value).
+Sample Input 1 
+10
+Sample Output 1
+40`
+
+let k = 10;
+k = k * 7;
+k = k + 10;
+k = k / 2;
+
+console.log(Math.floor(k)); //40
