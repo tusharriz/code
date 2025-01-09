746367e0a0fd2963f64b2e3a07fb450c3bafbbb4
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 11:48:42 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/27-check-greater.js
@@ -0,0 +1,12 @@
+`You are given two numbers, stored in the variable with the name x and y
+If x > y , then print "Greater"
+else print "Not Greater"`
+
+let x = 5;
+let y = 3;
+
+if(x > y){
+    console.log("Greater") // Greater
+}else{
+    console.log("Not Greater");
+}
