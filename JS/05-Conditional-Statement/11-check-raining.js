e853cd05d52fb7be132fba4a42a6319e70f0e075
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 11:38:14 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/11-check-raining.js
@@ -0,0 +1,9 @@
+// choosing whether to take an umbrella.
+
+let isRaining = true; // Assume it's raining
+
+if (isRaining) {
+    console.log("Take an umbrella."); // Take an umbrella
+} else {
+    console.log("No need for an umbrella.");
+}
