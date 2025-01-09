b17864e345ed9900168a83ee5dddd92e710d38fb
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 11:36:23 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/5-check-names.js
@@ -0,0 +1,8 @@
+//If the given two names are equal, print both names are equal
+
+let name1 = "chinapaa";
+let name2 = "chinapaa";
+
+if(name1==name2){
+    console.log("Both names are equal"); // Both names are equal
+}
