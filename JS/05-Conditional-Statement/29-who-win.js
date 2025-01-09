ff97c79f7623a7fd7271c31d15d558e3877410dd
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 11:50:52 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/29-who-win.js
@@ -0,0 +1,19 @@
+`You are given two numbers, scored in variables with the following names
+Australia, England
+If the following expression is true
+Australia > England, print "Australia"
+else if the following expression is true
+Australia < England, print "England"
+else if the following expression is true
+Australia == England, print "Tie"`
+
+let australia = 46;
+let england = 67;
+
+if(australia > england){
+    console.log("Australia");
+}else if(australia < england){
+    console.log("England"); // England
+}else if(australia == england){
+    console.log("Tie");
+}
