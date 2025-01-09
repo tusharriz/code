0084304e51cd80e60de417de60a32a34375a41c3
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 11:48:19 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/26-easy-wood-cutter.js
@@ -0,0 +1,12 @@
+`You are given a number, stored in a variable with the name N . Check if the the number is divisible by 3 or not
+If its possible, print "Yes"
+Else print "No"
+Hint : A number can be divided into 3 parts, if the number is completely divisible by 3, that is, the answer of the operation number % 3 is zero`
+
+let N = 6;
+
+if(N % 3 == 0){
+    console.log("Yes"); // Yes
+}else{
+    console.log("No");
+}
