bf94337f50d8bd26707c5c121c5b0f4245876fca
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 18:05:52 +0530

    Removed 1-and-&&-operator.js from tracking

--- JS/07-Logical-Operator/1-and-&&-operator.js
+++
@@ -1,6 +0,0 @@
-console.log(true && false); // false
-console.log(false && true); // false
-console.log(true && true); // true
-console.log(false && false); // false
-console.log(true && true && true && false); // false
-
