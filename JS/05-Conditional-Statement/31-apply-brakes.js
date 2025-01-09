77ae7088980535d09649ab5d04a4349bb026cfce
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 11:52:19 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/31-apply-brakes.js
@@ -0,0 +1,19 @@
+`You are given two numbers stored in the variable with the names
+distance, time
+Find the value of speed, such that,
+speed = distance/time
+If the following expression is true
+speed > 40, print "Apply Brake"
+else
+print "Keep Going"`
+
+let distand = 100;
+let time = 2;
+
+let speed = distand/time;
+
+if(speed > 40){
+    console.log("Apply Brakes");
+}else{
+    console.log("Keep Going");
+}
