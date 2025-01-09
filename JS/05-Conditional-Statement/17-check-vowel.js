7e8275ea53eb754d80280c200fedc2e3daa86fd2
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 11:39:55 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/17-check-vowel.js
@@ -0,0 +1,7 @@
+let vowel = 'z';
+
+if (vowel == 'a' || vowel == 'e' || vowel == 'i' || vowel == 'o' || vowel == 'u') {
+    console.log("Vowel");
+} else {
+    console.log("Not a vowel"); // Not a vowel
+}
