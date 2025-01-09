66813088aafe17ea9168ecbd26682895fd9bafbb
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 11:46:35 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/21-greater-or-both-equal.js
@@ -0,0 +1,12 @@
+// Given 2 numbers a and b print which is greater or "both equal".
+
+let a = 10;
+let b = 10;
+
+if (a > b) {
+    console.log("A is greater");
+} else if (b > a) {
+    console.log("B is greater");
+} else {
+    console.log("Both are equal"); // Both are equal
+}
