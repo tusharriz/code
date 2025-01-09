c7451fbb10e07958441560728b718f049f17497e
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 14:19:56 +0530

    coding exercise

---
+++ JS/07-Logical-Operator/2-check-marks.js
@@ -0,0 +1,10 @@
+`Imagine a scenario where a student must meet two criteria to qualify for an advanced course: a minimum grade of English at least 70 or more and Math minimum grade of at least 80.`
+
+let scienceGrade = 85;
+let mathsGrade = 92;
+
+if (scienceGrade >= 70 && mathsGrade >= 80) {
+    console.log("Qualified for the advanced course"); // Qualified for the advanced course
+} else {
+    console.log("Not qualified for the advanced course");
+}
