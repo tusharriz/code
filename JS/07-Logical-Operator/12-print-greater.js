d9bdf1c5238ce439b20067a3ab657b5ebb8a26d0
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 19:24:15 +0530

    coding exercise

---
+++ JS/07-Logical-Operator/12-print-greater.js
@@ -0,0 +1,13 @@
+`Given 3 numbers (all different values), print which is greatest`
+
+let a=10;
+let b=12;
+let c=18;
+
+if(a>b && a>c){
+  console.log(a,"is greater");
+} else if(b>a && b>c){
+  console.log(b, "is greater");
+} else {
+  console.log(c, "is greater");
+}
