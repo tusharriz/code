154f2b0aa2a80520b91c0b72d9a7443d80a1ef5b
Tushar Riz <tushariizzz@gmail.com>
2025-01-09 14:20:36 +0530

    coding exercise

---
+++ JS/07-Logical-Operator/3.1-guess-the-output.js
@@ -0,0 +1,12 @@
+
+if(5>3){
+  if(2>1){
+    console.log("both are greater"); // both are greater
+  }
+}
+
+//and now we can write this with the use of && operator
+
+if(5>3 && 2>1){
+    console.log("Both are greater"); // Both are greater
+}
