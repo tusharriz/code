7619031c701ec2455b3da7404b1f1d4cc69a0174
Tushar Riz <tushariizzz@gmail.com>
2025-01-10 13:27:25 +0530

    coding exercise

---
+++ JS/09-While-Loops/7-continue.js
@@ -0,0 +1,20 @@
+//continue : skip everything after me and go to starting of the loop.
+
+// for e.g.
+
+let i=1;
+
+while(i<=15){
+    if(i==4){
+        continue;
+    }
+    console.log(i);
+    i++;
+}
+
+// note: for stoping the infinite loop, press ctrl + c.
+
+// output:-
+// 1
+// 2
+// 3
