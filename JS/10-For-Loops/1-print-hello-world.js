891519c867291b7844ba151305fd0ba22cc66185
Tushar Riz <tushariizzz@gmail.com>
2025-01-10 18:50:41 +0530

    coding exercise

---
+++ JS/10-For-Loops/1-print-hello-world.js
@@ -0,0 +1,12 @@
+// Print "Hello World" 5 times using for loop
+
+for(let i=1; i<=5; i++){
+    console.log("Hello World");
+}
+
+//output
+// Hello World
+// Hello World
+// Hello World
+// Hello World
+// Hello World
