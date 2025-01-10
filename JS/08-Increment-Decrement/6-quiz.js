68a7ca91cce8ab1ef260fbc6ad14791adebb09d5
Tushar Riz <tushariizzz@gmail.com>
2025-01-10 11:49:41 +0530

    coding exercise

---
+++ JS/08-Increment-Decrement/6-quiz.js
@@ -0,0 +1,32 @@
+//pre-post-increment
+let a=10;
+let c=++a;
+let b=10;
+let d=b++;
+
+console.log(a); //11
+console.log(b); //11
+console.log(c); //11
+console.log(d); //10
+
+let m=10;
+let o=m++;
+let n=10;
+let p=++n;
+
+console.log(m); //11
+console.log(n); //11
+console.log(o); //10
+console.log(p); //11
+
+//pre-post-decrement
+
+let e=5;
+let f=e--;
+let g=10;
+let h=--g;
+
+console.log(e); //4
+console.log(f); //5
+console.log(g); //9
+console.log(h); //9
