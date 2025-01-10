527bfb11ac002b7fb6ff3df39d8e5b183990993c
Tushar Riz <tushariizzz@gmail.com>
2025-01-10 11:48:55 +0530

    coding exercise

---
+++ JS/08-Increment-Decrement/4-pre-post-increment.js
@@ -0,0 +1,25 @@
+//pre-increment
+
+let x = 10; // value of x is 10
+
+console.log(x); // 10
+
+let y = ++x; // 1. incrementing, 2. assigning
+
+// y = x=10+1 => 11 => so my x = 11 and now x assign to y so mean y = 11
+//pre increment means, increment the value first than assign. 
+
+console.log(y); // 11 
+console.log(x); // 11
+
+// 1. increment the value of x by 1 first
+// 2. then x assign into y
+
+//post-increment
+
+let a = 15; // value of a is 15
+
+let b = a++; // 1. assigning, 2. incrementing
+
+console.log(b); // 15
+console.log(a); // 16
