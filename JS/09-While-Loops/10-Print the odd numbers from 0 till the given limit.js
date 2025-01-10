0007e483c8e9a1f24057801b23018e6290b102bb
Tushar Riz <tushariizzz@gmail.com>
2025-01-10 13:28:09 +0530

    coding exercise

---
+++ JS/09-While-Loops/10-Print the odd numbers from 0 till the given limit.js
@@ -0,0 +1,19 @@
+// Print the odd numbers from 0 to 15
+
+let i = 0;
+
+while(i<=15){
+    if(i%2===1){
+        console.log(i);
+    }
+    i++;
+}
+// output:-
+// 1
+// 3
+// 5
+// 7
+// 9
+// 11
+// 13
+// 15
