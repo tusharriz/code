885df3aea56fd35d869d756258bb92acf6bfc74c
Tushar Riz <tushariizzz@gmail.com>
2025-01-11 13:15:47 +0530

    Delete 1-father-son-duo.js

--- JS/11-Nested-Loops/1-father-son-duo.js
+++
@@ -1,7 +0,0 @@
-//father son duo==> They own 5 farms, One day dather asked the son to plant 10 seeds in each farm, "*" is seed.
-
-for(let i=1; i<=5; i++){
-    for(let j=1; j<=10; j++){
-        console.log("farm",i,"seed",j);
-    }
-}
