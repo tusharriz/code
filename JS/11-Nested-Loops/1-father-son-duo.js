1c1eaa6ace635640d4ca37ee7f4592b0b3a48de6
Tushar Riz <tushariizzz@gmail.com>
2025-01-11 13:06:28 +0530

    coding exercise

---
+++ JS/11-Nested-Loops/1-father-son-duo.js
@@ -0,0 +1,7 @@
+//father son duo==> They own 5 farms, One day dather asked the son to plant 10 seeds in each farm, "*" is seed.
+
+for(let i=1; i<=5; i++){
+    for(let j=1; j<=10; j++){
+        console.log("farm",i,"seed",j);
+    }
+}
