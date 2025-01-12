bdf3a20f31b1000aedb78d635e30f20ccc26dccf
Tushar Riz <tushariizzz@gmail.com>
2025-01-13 00:03:59 +0530

    coding exercise

---
+++ JS/14-Coding-C1-Evaluation/04-Fizzz buzzz.js
@@ -0,0 +1,34 @@
+// Fizzz buzzz
+
+`You are given a number stored in a variable with the name num
+For all numbers in the range [1,num], including num
+1. If the number is divisible by 3 and 5 both, print "FizzBuzz" without quotes
+2. Else If the number is divisible by 3, print "Fizz", without quotes
+3. Else If the number is divisible by 5, print "Buzz", without quotes
+4. Else, print the number as it is 
+
+Sample Input 1 
+3
+Sample Output 1
+1
+2
+Fizz`
+
+let num = 3;
+
+for(let i=1; i<=num; i++){
+    if(i%3==0 && i%5==0){
+        console.log("FizzBuzz");
+    }else if(i%3==0){
+        console.log("Fizz");
+    }else if(i%5==0){
+        console.log("Buzz");
+    }else{
+        console.log(i);
+    }
+}
+
+`
+1
+2
+Fizz`
