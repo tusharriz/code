da71c65127dc96120f502d3b0af7dd929eccf265
Tushar Riz <tushariizzz@gmail.com>
2025-01-12 22:51:37 +0530

    coding exercise

---
+++ JS/13-Coding-Sprint-1-Mock-Evaluation-2/03-Compare Seven Numbers.js
@@ -0,0 +1,28 @@
+// Compare Seven Numbers
+
+`You are given seven numbers, stored in the variable with the following names
+a,b,c,d,e,f,g
+You have to declare two variables, sum1 and sum2 , such that sum1 = (a + b) * c and sum2 = (d + e + f + g)
+Print the output of the following operation
+sum1 > sum2
+
+Sample Input 1 
+1 2 3 4 5 6 7
+Sample Output 1
+false
+`
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+let f = 6;
+let g = 7;
+
+let sum1 = (a + b) * c;
+let sum2 = (d + e + f + g);
+
+console.log(sum1 > sum2);
+
+// false
