be60e3cfc26d4c772c9b5932bd276242bbcf6032
Tushar Riz <tushariizzz@gmail.com>
2025-01-12 22:51:24 +0530

    coding exercise

---
+++ JS/13-Coding-Sprint-1-Mock-Evaluation-2/02-Seven Numbers.js
@@ -0,0 +1,21 @@
+// Seven Numbers 
+
+`You are given 7 numbers A, B, C, D, E, F, G. Find out the product of (A + B + C) and (D + E + F + G).
+
+Sample Input 1 
+1 2 3 4 5 6 7
+Sample Output 1
+132
+`
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+let f = 6;
+let g = 7;
+
+console.log((a + b + c) * (d + e + f + g));
+
+// 132
