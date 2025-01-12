51044ff32246cf57115aba141f6ee5e7b6a7afdf
Tushar Riz <tushariizzz@gmail.com>
2025-01-12 12:46:18 +0530

    coding exercise

---
+++ JS/12-Coding-Sprint-1-Mock-Evaluation-1/07-Apply Brakes.js
@@ -0,0 +1,29 @@
+// Apply Brakes 
+
+`You are given two numbers stored in the variable with the names
+distance, time
+Find the value of speed, such that,
+speed = distance/time
+If the following expression is true
+speed > 40, print "Apply Brake"
+else
+print "Keep Going"
+Print the statements, without quotes
+
+Sample Input 1 
+100 2
+Sample Output 1
+Apply Brake`
+
+let distance = 100;
+let time = 2;
+
+let speed = distance/time;
+
+if(speed > 40){
+    console.log("Apply Brake");
+}else{
+    console.log("Keep Going");
+}
+
+// Apply Brake
