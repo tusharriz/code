1cd8f317c336c89adc2767e93351d612cff0b829
Tushar Riz <tushariizzz@gmail.com>
2025-01-12 22:52:44 +0530

    coding exercise

---
+++ JS/13-Coding-Sprint-1-Mock-Evaluation-2/05-Enough Fuel Consumption.js
@@ -0,0 +1,26 @@
+// Enough Fuel Consumption
+
+`You are given two numbers, stored in the variable with the following names
+fuel, distance
+Find the value of required, such that
+required = fuel*distance
+If the value of required is greater than 50, print Enough , else print Go On
+
+Sample Input 1 
+1 46
+Sample Output 1
+Go On
+`
+
+let fuel = 1;
+let distance = 46;
+
+let required = fuel * distance;
+
+if(required > 50){
+    console.log("Enough");
+}else{
+    console.log("Go On");
+}
+
+// Go On
