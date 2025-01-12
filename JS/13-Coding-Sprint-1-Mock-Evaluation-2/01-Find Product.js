4ad9888f02df25e2b29bdaeca3826bc3b081ad34
Tushar Riz <tushariizzz@gmail.com>
2025-01-12 22:50:08 +0530

    coding exercise

---
+++ JS/13-Coding-Sprint-1-Mock-Evaluation-2/01-Find Product.js
@@ -0,0 +1,20 @@
+// Find Product
+
+// You are given five numbers stored in variables with the following names
+// one, two, three, four, five
+// Find the product of the five values, and print it
+
+// Sample Input 1 
+// 1 2 3 4 5
+// Sample Output 1
+// 120
+
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+
+console.log(one * two * three * four * five);
+
+// 120
