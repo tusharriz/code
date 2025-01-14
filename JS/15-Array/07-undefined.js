98b9750b66b244b3c437299358994c3805b2cdf4
Tushar Riz <tushariizzz@gmail.com>
2025-01-14 21:04:23 +0530

    coding exercise

---
+++ JS/15-Array/07-undefined.js
@@ -0,0 +1,5 @@
+// undefined
+
+let vegNames = ["carrot","tomato","cucumber","potato"];
+console.log(vegNames[8]); // undefined => if the index not present in an array, it will give undefined.
+console.log(vegNames[-1]); // undefined => index cannot be negative.
