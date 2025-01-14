246c02e53ca1c4fb849895b1420153159c1e2948
Tushar Riz <tushariizzz@gmail.com>
2025-01-14 19:26:23 +0530

    coding exercise

---
+++ JS/15-Array/03-print-particular-element.js
@@ -0,0 +1,22 @@
+// 03-print-particular-element
+
+// let emp_names=["Clark", "Barry", "Bruce", "Tony"];
+
+//if you want to print all array elements;
+//do => console.log(emp_names);
+ 
+//[ 'Clark', 'Barry', 'Bruce', 'Tony' ]
+
+//but if you want to print a particular element like Clark only then you will use index.
+
+let emp_names=["Clark", "Barry", "Bruce", "Tony"];
+
+console.log(emp_names[0]);
+console.log(emp_names[1]);
+console.log(emp_names[2]);
+console.log(emp_names[3]);
+
+// Clark
+// Barry
+// Bruce
+// Tony
