ac9dd2d2edee185a0b9d97a0d783d1fde0d02b15
Tushar Riz <tushariizzz@gmail.com>
2025-01-14 19:42:56 +0530

    coding exercise

---
+++ JS/15-Array/05-length-of-an-array.js
@@ -0,0 +1,19 @@
+// 05-length-of-an-array
+
+// Print how many element in an array
+
+let vegNames = ["carrot","tomato","cucumber","potato"];
+
+console.log(vegNames.length); // 4
+
+//index =>            0        1         2         3
+// let vegNames = ["carrot","tomato","cucumber","potato"];
+//length =>           1        2         3         4
+
+//for getting last index of an array;
+
+
+// let vegNames = ["carrot","tomato","cucumber","potato"];
+console.log(vegNames.length-1); // 3
+console.log(vegNames.length-2); // 2
+console.log(vegNames.length-3); // 1
