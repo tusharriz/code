1f77f78ace4280723678da362f7994694b426ee6
Tushar Riz <tushariizzz@gmail.com>
2025-01-14 18:49:29 +0530

    coding exercise

---
+++ JS/15-Array/01-variables-vs-array-declaration.js
@@ -0,0 +1,12 @@
+// 01-variables-vs-array-declaration
+
+//declare a variable
+let emp1="Clark";
+let emp2="Barry";
+let emp3="Bruce";
+let emp4="Tony"; 
+
+//vs
+
+//declare an array
+let emp_names=["Clark", "Barry", "Bruce", "Tony"];
