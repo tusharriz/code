d4acd7c21704da61fd7cf73ad8e726ca1c32a036
Tushar Riz <tushariizzz@gmail.com>
2025-01-14 19:26:40 +0530

    coding exercise

---
+++ JS/15-Array/04-print-at-new-line.js
@@ -0,0 +1,16 @@
+// 04-print-at-new-line
+
+// declare an array, put the names of any 4 vegetables and print each vegetable in new line.
+
+let vegNames = ["carrot","tomato","cucumber","potato"];
+
+console.log(vegNames[0]);
+console.log(vegNames[1]);
+console.log(vegNames[2]);
+console.log(vegNames[3]);
+
+
+// carrot
+// tomato
+// cucumber
+// potato
