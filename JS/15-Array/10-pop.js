f9ed0abb429e63fa9c1fdb28787302385e715daf
Tushar Riz <tushariizzz@gmail.com>
2025-01-16 23:47:46 +0530

    coding exercise

---
+++ JS/15-Array/10-pop.js
@@ -0,0 +1,16 @@
+let veg=["Potato", "Tomato", "Egg Plant", "Cucumber"];
+
+// How to remove from the end
+// I will use "pop" to remove somethin g from the end of an array
+
+console.log("Before Popping the length was",veg.length); // 4 
+
+veg.pop();
+
+console.log("After Popping the length is",veg.length);// 3
+
+console.log(veg);
+
+// Before Popping the length was 4     
+// After Popping the length is 3       
+// [ 'Potato', 'Tomato', 'Egg Plant' ]
