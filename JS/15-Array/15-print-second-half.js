fef2e13b266a25cdd98a8396c21bdb9f7691b983
Tushar Riz <tushariizzz@gmail.com>
2025-01-16 23:49:10 +0530

    coding exercise

---
+++ JS/15-Array/15-print-second-half.js
@@ -0,0 +1,17 @@
+//print the second half of the array
+
+let arr=[1, 4, 5, 7, 8, 33, 88, 99];
+
+//print second half
+
+for(let i=arr.length/2; i<=arr.length-1; i++){
+    console.log(arr[i]);
+}
+
+// 8
+// 33
+// 88
+// 99
+
+//remember the length of the arr is even "8"
+//if length is not even it will give undefined
