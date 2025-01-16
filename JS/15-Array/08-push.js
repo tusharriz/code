db006ac5a44e5c1803fd5eb7efc60f42ce7f6e22
Tushar Riz <tushariizzz@gmail.com>
2025-01-16 19:11:38 +0530

    coding exercise

---
+++ JS/15-Array/08-push.js
@@ -0,0 +1,14 @@
+// push method
+
+// we have a method called as "push".
+// push will add the new element always at the end of the array.
+
+let veg=["Potato", "Tomato", "Egg Plant", "Cucumber"];
+console.log(veg);
+console.log(veg.length);
+
+veg.push("Carrot");
+console.log(veg);
+console.log(veg.length);
+
+
