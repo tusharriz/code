cb1e95344275966ecb717a545192c986d26d97bd
Tushar Riz <tushariizzz@gmail.com>
2025-01-19 22:46:47 +0530

    coding exercise

---
+++ JS/18-Object/02-object-vs-array.js
@@ -0,0 +1,18 @@
+// Object vs Array
+
+//  declaring an array
+let arr  = ["Ram", 26, "male", "Delhi", "coding"];
+console.log(arr[3]); // Delhi
+
+
+// declaring an object
+
+let obj = {
+    name : "Ram",
+    age : 26,
+    gender : "male",
+    city : "Delhi",
+    hobbies : "coding"
+}
+
+console.log(obj["city"]); // Delhi
