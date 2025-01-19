020b23d44ee089edb3ec54f71e28e73b650f522d
Tushar Riz <tushariizzz@gmail.com>
2025-01-19 13:29:22 +0530

    coding exercise

---
+++ DSA/02-DSA -Contest-II/02-Equal to 42 or not!.js
@@ -0,0 +1,20 @@
+// Equal to 42 or not!
+
+function equalTo42(arr) {
+    //write code here
+    let flag = false;
+    
+    for(let i=0; i<=arr.length-1; i++){
+        if(arr[i] === 42){
+            flag = true;
+        }
+    }
+    
+    if(flag === true){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
+}
+
+equalTo42([3,7,0,9,8]); // No
