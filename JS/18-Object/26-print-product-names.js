a01d1efc354a78e1f83558f82171576158c327fb
Tushar Riz <tushariizzz@gmail.com>
2025-01-21 16:28:12 +0530

    coding exercise

---
+++ JS/18-Object/26-print-product-names.js
@@ -0,0 +1,21 @@
+// print those product names whose rating in more than 3
+
+let products = [
+  { name: "macbook", price: 150000, rating: 4.5 },
+  { name: "iphone 13", price: 71000, rating: 4 },
+  { name: "airpods", price: 20000, rating: 3.8 },
+  { name: "charger", price: 400, rating: 2.5 },
+  { name: "ipad", price: 55000, rating: 3 },
+];
+
+let obj = {};
+
+for(let i=0; i<=products.length-1; i++){
+    if(products[i]["rating"] > 3){
+        console.log(products[i]["name"]);
+    }
+}
+
+// macbook
+// iphone 13
+// airpods
