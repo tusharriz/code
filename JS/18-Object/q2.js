738ef0fa5327839318437aea72674ec13217af5d
Tushar Riz <tushariizzz@gmail.com>
2025-01-22 18:50:59 +0530

    coding exercise

---
+++ JS/18-Object/q2.js
@@ -0,0 +1,15 @@
+// synatx of Object :
+
+// let obj = {
+//     key1 : "Value"
+// };
+// console.log(obj);
+
+let details = {
+    name : "Nobita",
+    age : 19,
+    location : "Japan",
+    role : "Developer"
+};
+
+console.log(details);
