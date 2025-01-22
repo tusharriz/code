536a4e22145445f4ca3982dbbc7b49781b490bec
Tushar Riz <tushariizzz@gmail.com>
2025-01-22 14:08:32 +0530

    coding exercise

---
+++ JS/20-Coding-Sprint-2-Mock-Evaluation-1/03-Mod of 6.js
@@ -0,0 +1,20 @@
+// Mod of 6
+
+function modOf6(num){
+	//Write your code here
+	let x = num % 6;
+	if(x==0){
+	    console.log("zero");
+	}else if(x==1){
+	    console.log("one");
+	}else if(x==2){
+	    console.log("two");
+	}else if(x==3){
+	    console.log("three");
+	}else if(x==4){
+	    console.log("four");
+	}else if(x==5){
+	    console.log("five");
+	}
+}
+modOf6(7); // one
