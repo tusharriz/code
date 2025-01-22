f16f211b448e5370ec9ddf6ed01ad4657947d497
Tushar Riz <tushariizzz@gmail.com>
2025-01-22 18:50:33 +0530

    coding exercise

---
+++ JS/18-Object/q1.js
@@ -0,0 +1,12 @@
+// access the value of a particular key
+
+let details = {
+    name : "Nobita",
+    age : 19,
+    location : "Japan",
+    role : "Developer"
+};
+
+// bracket notation
+console.log(details["name"]);
+console.log(details["role"]);
