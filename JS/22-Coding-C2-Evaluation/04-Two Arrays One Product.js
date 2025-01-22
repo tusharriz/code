7c46b6f3d90b778e93cab33e901bbdd6763d792f
Tushar Riz <tushariizzz@gmail.com>
2025-01-22 23:58:34 +0530

    coding exercise

---
+++ JS/22-Coding-C2-Evaluation/04-Two Arrays One Product.js
@@ -0,0 +1,17 @@
+// Two Arrays One Product
+
+function twoArraysOneProduct(A, B) {
+  //write code here
+  let a = 1;
+  let b = 1;
+  for (let i = 0; i <= A.length - 1; i++) {
+    a = a * A[i];
+  }
+
+  for (let j = 0; j <= B.length - 1; j++) {
+    b = b * B[j];
+  }
+
+  console.log(a * b);
+}
+twoArraysOneProduct([1,2,3],[3,2,1]); //36
