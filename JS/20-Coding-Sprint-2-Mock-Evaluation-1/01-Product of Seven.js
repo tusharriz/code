86ba657511ea126ba8579094f9940980af851ee1
Tushar Riz <tushariizzz@gmail.com>
2025-01-22 14:07:39 +0530

    coding exercise

---
+++ JS/20-Coding-Sprint-2-Mock-Evaluation-1/01-Product of Seven.js
@@ -0,0 +1,8 @@
+// Product of Seven
+
+function productOfFive(one, two, three, four, five, six, seven){
+	//Enter Code Here
+	console.log(one*two*three*four*five*six*seven);
+
+}
+productOfFive(1, 2, 3, 4, 5, 6, 7); // 5040
