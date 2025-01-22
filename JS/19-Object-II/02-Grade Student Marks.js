e352431259d4ce2be5d21ee7b08c75acaa28153c
Tushar Riz <tushariizzz@gmail.com>
2025-01-22 14:10:22 +0530

    coding exercise

---
+++ JS/19-Object-II/02-Grade Student Marks.js
@@ -0,0 +1,33 @@
+// Grade Student Marks
+`
+- Given an object of student grades and their marks in the mentioned below format, print the highest scored student for each grade along with the total
+
+Sample Output
+V-Prateek-90
+VI-Albert-90
+VII-Sandhya-90`;
+
+let arr = [
+  {
+    grade: "V",
+    students: [
+      { name: "Nrupul", marks: [10, 20, 30] },
+      { name: "Prateek", marks: [20, 30, 40] },
+    ],
+  },
+  {
+    grade: "VI",
+    students: [
+      { name: "Aman", marks: [10, 20, 30] },
+      { name: "Albert", marks: [20, 30, 40] },
+    ],
+  },
+  {
+    grade: "VII",
+    students: [
+      { name: "Yogesh", marks: [10, 20, 30] },
+      { name: "Sandhya", marks: [20, 30, 40] },
+    ],
+  },
+];
+
