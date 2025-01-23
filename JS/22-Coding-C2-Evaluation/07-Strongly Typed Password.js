fdd5be1ac4458ba5c81128f6409b360eb24f00b0
Tushar Riz <tushariizzz@gmail.com>
2025-01-23 21:00:58 +0530

    coding exercise

---
+++ JS/22-Coding-C2-Evaluation/07-Strongly Typed Password.js
@@ -0,0 +1,90 @@
+// Strongly Typed Password
+
+let str = "neat";
+let symbol = "@$#*";
+let bag = "";
+let flag = false;
+
+// {
+//   // solved for only 1 string with OR (||) logical operator
+//   for(let i=0; i<=str.length-1; i++){
+//     // console.log(str[i]);
+//     if(str[i] == "@" || str[i] == "$" || str[i] == "#" || str[i] == "*"){
+//       bag = bag + "strong";
+//       flag = true;
+//       break;
+//     }
+//   }
+//   // console.log(bag);
+//   if(flag == false){
+//     console.log("weak");
+//   }
+// }
+
+// {
+//   // solved for only 1 string with nested loop
+//   for (let i = 0; i <= str.length - 1; i++) {
+//     // console.log(str[i]);
+//     for (let j = 0; j <= symbol.length - 1; j++) {
+//       if (str[i] == symbol[j]) {
+//         bag = bag + "strong";
+//         flag = true;
+//         break;
+//       }
+//     }
+//   }
+//   // console.log(bag);
+
+//   if (flag == false) {
+//     console.log("weak");
+//   }
+// }
+
+// {
+//   let arr = ["ab","t@1"];
+//   let symbol = "#@";
+//   let bag = "";
+
+//   for(let i=0; i<=arr.length-1; i++){
+//     let str = arr[i];
+
+//     for(let j=0; j<=str.length-1; j++){
+//       if(str[j] == "#" || str[j] == "@"){
+//         bag = bag + "strong" + " ";
+//         flag = true;
+//         break;
+//       }
+//     }
+//     if(flag == false){
+//       bag = bag + "weak" + " ";
+//     }
+//   }
+//   console.log(bag);
+// }
+
+function strongPassword(arr) {
+  //write code here
+
+  let symbol = "@$#*";
+  let bag = "";
+
+  for (let i = 0; i <= arr.length - 1; i++) {
+    let flag = false;
+    let str = arr[i];
+  
+    for (let j = 0; j <= str.length - 1; j++) {
+      if (str[j] == "@" || str[j] == "$" || str[j] == "#" || str[j] == "*") {
+        bag = bag + "strong" + " ";
+        flag = true;
+        break;
+      }
+    }
+    if (flag === false) {
+      bag = bag + "weak" + " ";
+    }
+  }
+  console.log(bag);
+}
+strongPassword(["test123", "new@t", "mon*y", "nrupul"]);
+
+// weak strong strong weak
