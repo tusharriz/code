71d056b1d4de1fbddd71470c2560d6225671ff4a
Tushar Riz <tushariizzz@gmail.com>
2025-01-26 23:05:06 +0530

    coding exercise

---
+++ JS/23-Function-I/09-write a function to perform multiplication and use return statement.js
@@ -0,0 +1,16 @@
+// write a function to perform multiplication and use return statement
+
+function multiplication(a,b){
+    return a*b;
+}
+
+console.log(multiplication(5,10)) // 50
+
+// or
+
+let result = multiplication(5,10);
+console.log(result) ; // 50
+
+//1-> Return is going to give back the answer to the calling statement.
+
+//2-> As soon as compiler encounters a return statemnet, it will take the answer and go out of the function
