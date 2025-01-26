046aad903cf5fbe4292156234516511390e196cf
Tushar Riz <tushariizzz@gmail.com>
2025-01-26 23:05:31 +0530

    coding exercise

---
+++ JS/23-Function-I/11-odd-function.js
@@ -0,0 +1,12 @@
+// write a function for odd
+
+function even(num){
+    if(num%2!==0){
+        return "Odd";
+    }else{
+        return "Not Odd";
+    }
+}
+
+console.log(even(5)); // Odd
+console.log(even(2)); // Not Odd
