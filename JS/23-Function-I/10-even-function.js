4daa88e2fb3b45a1abe19a959a969f34a0c3d921
Tushar Riz <tushariizzz@gmail.com>
2025-01-26 23:05:20 +0530

    coding exercise

---
+++ JS/23-Function-I/10-even-function.js
@@ -0,0 +1,12 @@
+// write a function for even
+
+function even(num){
+    if(num%2===0){
+        return "Even";
+    }else{
+        return "Not Even";
+    }
+}
+
+console.log(even(5)); // Not Even
+console.log(even(2)); // Even
