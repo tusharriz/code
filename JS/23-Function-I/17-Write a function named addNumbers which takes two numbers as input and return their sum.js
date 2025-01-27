d94495d2198b563dac06983d952ec01ac003d923
Tushar Riz <tushariizzz@gmail.com>
2025-01-27 23:14:15 +0530

    coding exercise

---
+++ JS/23-Function-I/17-Write a function named addNumbers which takes two numbers as input and return their sum.js
@@ -0,0 +1,14 @@
+// Write a function named addNumbers which takes two numbers as input and return their sum
+
+function addNumbers(num1, num2){
+
+    let sum = num1 + num2;
+    return sum;
+}
+console.log(addNumbers(5,7)); // 12
+
+// Use the above function to calculate the sum of given three numbers 7, 6, 15;
+
+let num3 = 15;
+console.log(num3 + addNumbers(7,6)); // 28 
+
