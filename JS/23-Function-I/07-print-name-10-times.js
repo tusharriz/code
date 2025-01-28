d776806286c56976697dbc9f0f871a361a2ad23d
Tushar Riz <tushariizzz@gmail.com>
2025-01-28 17:22:02 +0530

    coding exercise

---
+++ JS/23-Function-I/07-print-name-10-times.js
@@ -0,0 +1,18 @@
+function fullName(firstName, lastName){
+    console.log(firstName + " " + lastName);
+}
+
+for(let i=1; i<=10; i++){
+    fullName("Tushar", "Riz"); // Tushar Riz
+}
+
+// Tushar Riz
+// Tushar Riz
+// Tushar Riz
+// Tushar Riz
+// Tushar Riz
+// Tushar Riz
+// Tushar Riz
+// Tushar Riz
+// Tushar Riz
+// Tushar Riz
