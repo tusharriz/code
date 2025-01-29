a919a9353902ef621bd564fc6b1db12c4cbbf93e
Tushar Riz <tushariizzz@gmail.com>
2025-01-29 19:57:23 +0530

    coding exercise

---
+++ DSA/07-DSA-Contest-VI/01-Count such pairs.js
@@ -0,0 +1,14 @@
+function countSuchPairs(A,K){
+    //write code here
+    let count = 0;
+    
+    for(let i=0; i<A.length; i++){
+        for(let j=i+1; j<A.length; j++){
+            if(A[i] + A[j] === K){
+                count++;
+            }
+        }
+    }
+    console.log(count);
+}
+countSuchPairs([3,0,6,2,7],9); // 2
