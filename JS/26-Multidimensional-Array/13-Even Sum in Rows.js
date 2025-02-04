71e22c9f410dd808d323a26f0ae087b461f6e4bf
Tushar Riz <tushariizzz@gmail.com>
2025-02-05 00:34:37 +0530

    coding exercise

---
+++ JS/26-Multidimensional-Array/13-Even Sum in Rows.js
@@ -0,0 +1,19 @@
+// Even Sum in Rows
+
+function evenSumRows(N,M,arr){
+    //write code here
+    
+    for(let i=0; i<=N-1; i++){
+        let sum = 0;
+        for(let j=0; j<=M-1; j++){
+            if(arr[i][j]%2===0){
+                sum += arr[i][j];
+            }
+        }
+        console.log(sum);
+    }
+}
+evenSumRows(3,3,[[1,2,3],[4,5,6],[7,8,9]]);
+// 2
+// 10
+// 8
