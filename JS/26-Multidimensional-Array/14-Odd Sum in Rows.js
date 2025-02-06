1e76b1e50459a2bac7d316153822a79224c1aa73
Tushar Riz <tushariizzz@gmail.com>
2025-02-06 19:36:17 +0530

    codind exercise

---
+++ JS/26-Multidimensional-Array/14-Odd Sum in Rows.js
@@ -0,0 +1,21 @@
+// Odd Sum in Rows
+
+function oddSumRows(N,M,arr){
+    //write code here
+    
+    for(let i=0; i<=N-1; i++){
+        let sum = 0;
+        for(let j=0; j<=M-1; j++){
+            if(arr[i][j]%2!==0){
+                sum += arr[i][j];
+            }
+        }
+        console.log(sum);
+    }
+    
+}
+oddSumRows(3,3,[[1,2,3],[4,5,6],[7,8,9]])
+
+// 4
+// 5
+// 16
