35a3bdbfb5ce6caff5264a342aa4985ffd831a9d
Tushar Riz <tushariizzz@gmail.com>
2025-02-08 23:21:09 +0530

    coding exercise

---
+++ JS/28-Coding Sprint 3 Mock Evaluation 2/03-Map Symbols.js
@@ -0,0 +1,30 @@
+// Map Symbols
+
+function mapSymbols(N) {
+    // Write code here
+    
+    let symbol = "!@#$%^&*";
+    let obj = {};
+    
+    for(let i=0; i<=symbol.length-1; i++){
+        if(obj[symbol[i]] === undefined){
+            obj[symbol[i]] = N;
+            N=N+2;
+        }
+    }
+    // console.log(obj);
+    
+    for(let key in obj){
+        console.log(key+"-"+obj[key]);
+    }
+}
+mapSymbols(20); 
+
+// !-20
+// @-22
+// #-24
+// $-26
+// %-28
+// ^-30
+// &-32
+// *-34
