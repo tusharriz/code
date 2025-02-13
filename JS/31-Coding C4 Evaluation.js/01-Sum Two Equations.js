02f48e0b363809cd56941bd40d962d6b1abfef4a
Tushar Riz <tushariizzz@gmail.com>
2025-02-13 23:44:20 +0530

    coding exercise

---
+++ JS/31-Coding C4 Evaluation.js/01-Sum Two Equations.js
@@ -0,0 +1,10 @@
+// Sum Two Equations
+
+function sumTwoEqs(a, b) {
+  // write code here
+  let x = a * 5 + b * 3;
+  let y = a * 7 + b * 4;
+  let sum = x + y;
+  console.log(sum);
+}
+sumTwoEqs(2, 3); // 45
