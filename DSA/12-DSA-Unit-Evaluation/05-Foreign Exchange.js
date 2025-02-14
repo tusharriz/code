01775a2fb466fec55cb9cd83938e637d90acd81f
Tushar Riz <tushariizzz@gmail.com>
2025-02-15 00:16:39 +0530

    coding exercise

---
+++ DSA/12-DSA-Unit-Evaluation/05-Foreign Exchange.js
@@ -0,0 +1,10 @@
+// Foreign Exchange
+
+function foreign_exchange(N) {
+    // Write code here
+    let dinar = 18;
+    let convert = N * dinar;
+    console.log(convert);
+    
+}
+foreign_exchange(50); // 900
