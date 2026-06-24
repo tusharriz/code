cce8908f5ee3657d04efda1843e14c27c6dd40a0
Tushar Riz <er.tusharriz@gmail.com>
2026-06-24 23:26:30 +0530

    coding exercise

--- JS/01-Variables And Data Type/02-case-sensitive.js
+++ JS/01-Variables And Data Type/02-case-sensitive.js
@@ -7,3 +7,8 @@ let A = 50;
 console.log(X);
 
  // both a and A varibale are different.
+ // x and X both are not defined.
+ // if you print the value of a or A, so replace x or X with a or A inside the console.
+
+ console.log(a);
+ console.log(A);
