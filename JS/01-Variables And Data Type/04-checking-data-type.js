c1872f84a06db491c436188bd5e17cedde558499
Tushar Riz <er.tusharriz@gmail.com>
2026-06-24 23:27:14 +0530

    coding exercise

---
+++ JS/01-Variables And Data Type/04-checking-data-type.js
@@ -0,0 +1,36 @@
+// print datatype of variable.
+
+let x = 50; // the value of x is number.
+let y = "Ram"; // the value of y is string.
+let z = "101" // the value of z is also a string.
+
+//Note : whatever data is present inside the double quotes, it will always be a string.
+
+console.log(x); // 50
+console.log(y); // Ram
+console.log(z); // 101
+
+// In Number datatype, Numbers can store both Whole Numbers/Integers and Decimals.
+
+let num = 100;
+let dec = 1002.1;
+let int = -52;
+
+// for checking datatype of the value;
+
+console.log(typeof(x)); // number datatype
+console.log(typeof(y)); // string datatype
+console.log(typeof(z)); // string datatype
+console.log("sdg53#$@fg 5"); // this value is also a string datatype 
+// string can be a combination of num, character, symbol and " " <--- space.
+
+let check = typeof(z);
+console.log(check); // string
+
+// A space is also a character in JS. So any space inside the double quotes is also a string.
+let a = " ";
+console.log(a); // blank
+console.log(typeof(a)); // string
+
+
+
