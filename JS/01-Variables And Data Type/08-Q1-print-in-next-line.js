cd56e9a1438a2da9cab39fd8d0ccf8e191927e03
Tushar Riz <er.tusharriz@gmail.com>
2026-06-25 12:36:47 +0530

    coding exercise

---
+++ JS/01-Variables And Data Type/08-Q1-print-in-next-line.js
@@ -0,0 +1,11 @@
+// Q1-print-in-next-line
+// print JS, DSA and WEB-DEV in the next line but use only one "console.log" statement.
+//JS
+//DSA
+//WEB-DEV
+
+console.log("JS" + '\n' + "DSA" + '\n' + "WEB-DEV");
+// or
+console.log("JS\nDSA\nWEB-DEV");
+
+// next line mei print karne ke liye \n ka use krte hai.
