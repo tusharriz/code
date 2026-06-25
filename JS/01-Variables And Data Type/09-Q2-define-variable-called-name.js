bd6b6918ea043a30dd45b26b72e5d7d8e9d5d4e4
Tushar Riz <er.tusharriz@gmail.com>
2026-06-25 12:37:32 +0530

    coding exercise

---
+++ JS/01-Variables And Data Type/09-Q2-define-variable-called-name.js
@@ -0,0 +1,10 @@
+// Q2-define-variable-called-name
+
+let name = "Ram";
+console.log(name);
+
+name = "Ramanujan";
+console.log(name);
+
+name = "Rama";
+console.log(name);
---
+++ JS/01-Variables And Data Type/10-Q3-print-in-the-same-line-and-next-line.js
@@ -0,0 +1,9 @@
+// Q3-print-in-the-same-line-and-next-line
+// Create a variable 'name' to store your name 
+// Create a variable 'age' to store your age 
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+let name = "Hari";
+let age = 26;
+
+console.log(name + " " + age + "\n" + typeof name + " " +typeof age );
