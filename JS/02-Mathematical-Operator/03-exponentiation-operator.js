af3ff0a88c3caa2e4673c71292b0dc030ff0babc
Tushar Riz <er.tusharriz@gmail.com>
2026-06-26 18:20:57 +0530

    coding exercise

--- JS/02-Mathematical-Operator/03-exponentiation-operator.js
+++ JS/02-Mathematical-Operator/03-exponentiation-operator.js
@@ -6,4 +6,14 @@ let b = 2;
 // a*b => 10*2 => 10x2 = 20;
 //a**b => 10**2 => 10x10 = 100;
 
-console.log(10**2); // 100
+console.log(10**2); // 100 
+
+// This operator is represented by ** . This returns the value of the first operand raised to the power of the second operand.
+
+let m = 2**4; // 2*2*2*2
+let n = 3**2; // 3*3
+let o = 10**1.5;
+
+console.log(m); // 16
+console.log(n); // 9 
+console.log(o); // 31.622776601683793
