134cfda85e06384bb017d764fa9a85c31367c6b4
Tushar Riz <er.tusharriz@gmail.com>
2026-06-26 23:31:08 +0530

    coding exercise

---
+++ JS/03-Relation-Operator/2.2-passed-or-not.js
@@ -0,0 +1,10 @@
+`Check whether Ram has failed the exam where the passing marks are 35 using the < operator.
+
+true ==> YES
+false ==> NO
+`
+let ramMarks = 34;
+let passingMarks = 35;
+
+let ramIsFailed = ramMarks < passingMarks;
+console.log("Ram is Failed =", ramIsFailed); // true
