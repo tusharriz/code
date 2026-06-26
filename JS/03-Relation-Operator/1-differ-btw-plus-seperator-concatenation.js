9f4c31685eab050c078bf593d5bf9ecf0d13d36f
Tushar Riz <er.tusharriz@gmail.com>
2026-06-26 23:28:31 +0530

    coding exercise

---
+++ JS/03-Relation-Operator/1-differ-btw-plus-seperator-concatenation.js
@@ -0,0 +1,8 @@
+let a = "Ram";
+let b = "Lakhan";
+
+console.log(a+b); //RamLakhan <-- addition
+console.log(a,b); //Ram Lakhan <-- seperator
+console.log(a+" "+b); //Ram Lakhan <-- concatenation
+
+
