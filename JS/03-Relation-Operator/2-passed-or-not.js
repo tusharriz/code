f1bdc439305a0551c61da639c69a78543649c517
Tushar Riz <er.tusharriz@gmail.com>
2026-06-26 23:29:58 +0530

    coding exercise

---
+++ JS/03-Relation-Operator/2-passed-or-not.js
@@ -0,0 +1,10 @@
+`Check whether Ram marks are enough to pass where the passing mark is 35 using the > operator.
+
+true ==> YES
+false ==> NO
+`
+let ramMarks = 36; 
+let passingMarks=35;
+
+let ramIsPassed = ramMarks >= passingMarks;
+console.log("Ram is Passed =", ramIsPassed); // true
