335e4117054d950a2c1d0e7129ce71bf0d71fcd3
Tushar Riz <er.tusharriz@gmail.com>
2026-06-26 13:39:00 +0530

    coding exercise

---
+++ JS/02-Mathematical-Operator/02-modulo-operator.js
@@ -0,0 +1,25 @@
+// This is a modulo operator "%"
+
+let a = 10;
+let b = 2;
+
+console.log(a/b); // ==>  2|10|5 ==> 5 is quotient
+//                          10
+//                           0
+
+console.log(a%b); // ==>  2|10|5
+//                          10
+//                           0  ==> 0 is remainder
+
+// we need remainder only to solve coding questions
+
+console.log(10%2); // ==>  2|10|5
+//                           10
+//                            0  ==> 0 is remainder
+
+// we need remainder only to solve coding questions
+
+console.log(10%2); // 0
+console.log(2%10); // 2 ==>  10|2|0
+//                              0
+//                              2  ==> 2 is remainder
