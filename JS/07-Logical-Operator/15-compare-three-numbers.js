f808e698518e71b24bd0c2d5b60bb71f75d2509e
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 22:36:56 +0530

    coding exercise

---
+++ JS/07-Logical-Operator/15-compare-three-numbers.js
@@ -0,0 +1,18 @@
+`You are given three numbers stored in variables with the following names
+one, two, three
+If the value of both the following expression is true, print Yes , else print No
+1. one > two
+2. one > three
+
+Sample Input 1 
+6 2 4
+Sample Output 1
+Yes`
+
+let one = 6, two = 2, three = 4;
+
+if(one > two && one > three){
+    console.log("Yes"); // Yes
+}else{
+    console.log("No"); 
+}
