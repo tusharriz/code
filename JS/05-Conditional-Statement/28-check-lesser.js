1b7fb641ad616e67ad989233131f542a505257e5
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 20:58:37 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/28-check-lesser.js
@@ -0,0 +1,12 @@
+`You are given two numbers, stored in the variable with the name x and y
+If x < y , then print "Lesser"
+else print "Not Lesser"`
+
+let x = 2;
+let y = 5;
+
+if(x < y){
+    console.log("Lesser");
+}else{
+    console.log("Not Lesser"); // Lesser
+}
