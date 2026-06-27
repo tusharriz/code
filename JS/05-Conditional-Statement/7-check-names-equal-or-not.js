07ad3432c9265d623ec064b996b745f103c8bc36
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 20:52:40 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/7-check-names-equal-or-not.js
@@ -0,0 +1,27 @@
+// There are two students, named Ram and Lakhan, if the names are equal then print "same student" otherwise print not same.
+
+let studentName1 = "Nima";
+let studentName2 = "Nima";
+
+if(studentName1 === studentName2){
+    console.log("Same Student"); // Same Student
+  } else {
+    console.log("Not Same");
+  }
+
+// Or, we can directly write the names inside the condition.
+
+if("Ram"=="Lakhan"){
+    console.log("Same Student");
+  } else {
+    console.log("Not Same"); // Not Same
+}
+
+// we can also check numbers
+
+if(5 == 4){
+    console.log("Same");
+  } else {
+    console.log("Not Same"); // Not Same
+}
+
