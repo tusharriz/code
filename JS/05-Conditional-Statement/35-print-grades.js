0dc281f93c9bfd1b934e671bab89f6f73b793358
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 21:00:32 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/35-print-grades.js
@@ -0,0 +1,23 @@
+`You are given a number, stored in the variable with the name total
+-If the following expression is true
+-total == 100, print "A", without quotes
+
+-Else if the following expression is true,
+-total >= 90, print "B", without quotes
+
+-Else if the following expression is true,
+-total >= 80, print "C", without quotes
+
+-Else, print "F", without quotes`
+
+let total = 80;
+
+if(total == 100){
+    console.log("A");
+}else if(total >= 90){
+    console.log("B");
+}else if(total >= 80){
+    console.log("C"); // C
+}else{
+    console.log("F");
+}
