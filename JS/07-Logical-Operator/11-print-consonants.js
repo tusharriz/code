164e27129f05b665293971d9876063a923fd7328
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 22:35:56 +0530

    coding exercise

---
+++ JS/07-Logical-Operator/11-print-consonants.js
@@ -0,0 +1,7 @@
+// Given and character if it is a consonant print "Consonant"
+
+let char = "b";
+
+if(char == "b" || char == "c" || char == "d" || char == "f" || char == "g" || char == "h" || char == "j" || char == "k" || char == "l" || char == "m" || char == "n" || char == "p" || char == "q" || char == "r" || char == "s" || char == "t" || char == "v" || char == "w" || char == "x" || char == "y" || char == "z"){
+    console.log("Consonant");
+}
