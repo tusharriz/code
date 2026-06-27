cf50f111c081174eef1f97ac587c61dfeeef90a4
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 20:57:21 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/23-Equal-to-2.js
@@ -0,0 +1,10 @@
+`You are given a number, stored in the variable with the name N
+Print Yes , if the number is equal to 2, else print No.`
+
+let N = 2;
+
+if(N == 2){
+    console.log("Yes"); // Yes
+}else{
+    console.log("No");
+}
