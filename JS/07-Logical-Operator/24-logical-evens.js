8deb6ca2fbc157bdbd59528a5029fa749469cd8d
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 22:39:34 +0530

    coding exercise

---
+++ JS/07-Logical-Operator/24-logical-evens.js
@@ -0,0 +1,17 @@
+`You are given two numbers, stored in the variable with the following names
+one, two
+If both the values are Even, print Both , else print No
+
+Sample Input 1 
+4 7
+Sample Output 1
+No
+`
+
+let one = 4, two = 7;
+
+if(one%2==0 && two%2==0){
+    console.log("Both");
+}else{
+    console.log("No"); // No
+}
