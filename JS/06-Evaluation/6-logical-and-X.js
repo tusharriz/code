458592f010878c2f30476cec0630ae8df55d2f9e
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 21:26:25 +0530

    coding exercise

---
+++ JS/06-Evaluation/6-logical-and-X.js
@@ -0,0 +1,13 @@
+`
+You are given two numbers stored in variables with the name, a and b
+Print the result of the following operation
+a >= 15 && b < 25
+Sample Input 1 
+16 21
+Sample Output 1
+true`
+
+let a = 16;
+let b = 21;
+
+console.log( a >= 15 && b < 25); // true
