a688ffdcc3917ae0fd49f9648e529ee9d2c4cd58
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 22:36:31 +0530

    coding exercise

---
+++ JS/07-Logical-Operator/14-compare-two-numbers.js
@@ -0,0 +1,17 @@
+`You are given two numbers stored in variables with the following names
+one, two
+If the value of the following expression is true, print Yes , else print No
+one > two `
+
+`Sample Input 1 
+5 6
+Sample Output 1
+No`
+
+let one = 5, two = 6;
+
+if(one > two){
+    console.log("Yes");
+}else{
+    console.log("No"); //No
+}
