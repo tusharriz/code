db5074b8a23469c1e77a1eab69939f6563da6bba
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 21:25:21 +0530

    coding exercise

---
+++ JS/06-Evaluation/1-problem.js
@@ -0,0 +1,10 @@
+`Problem 1: Ajay went for shopping one day and decided to visit a showroom of Nike. While entering the store, he saw an offer on the door, The offer was a customer would get a discount of 10% when his/her bill total is 5000 or above. Consider Ajay purchases products worth of 6000, will he get the discount or not?
+- Declare the appropriate variable and get the result.`
+
+let totalBill = 6000;
+
+if(totalBill >= 5000){
+    console.log("Discount of 10%"); // Discount of 10%
+}else{
+    console.log("Not Eligible for a Discount")
+}
