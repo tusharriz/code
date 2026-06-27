6f61a82c8aefbcf9e3c54648331ae30fd7720076
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 20:51:25 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/3-guess-the-output.js
@@ -0,0 +1,9 @@
+//guess the output
+
+console.log("Code Starts"); //print
+
+if(false){
+  console.log("Inside the statemnet"); //this is not print because the condition is false
+}
+
+console.log("Code ends"); //print
