2c0cb676daf0eaac1c0f721c71e6c5e11f215ba3
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 19:57:39 +0530

    coding exercise

---
+++ JS/04-Comparison-Operator/5-not-double-equal-to.js
@@ -0,0 +1,9 @@
+//not double equal to (!==)
+// - true : It gives “true” only when the first value or data type is not equal to the second value or data type.
+
+console.log(5 !== "5") // true => number type of 5 is not equal to string type of 5.
+
+
+// - false : It gives “false” only when the first value or data type is equal to the second value or data type.
+
+console.log("5" !== "5") // false => it say's string type of 5 is not equal to string type of 5.
