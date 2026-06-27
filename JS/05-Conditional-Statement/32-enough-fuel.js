a3d27c4841ea1697beeb3059dea1d0a3337a2fde
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 20:59:48 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/32-enough-fuel.js
@@ -0,0 +1,16 @@
+`You are given two numbers, stored in the variable with the following names
+fuel, distance
+Find the value of required, such that
+required = fuel*distance
+If the value of required is greater than 50, print Enough , else print Go On`
+
+let fuel = 1;
+let distance = 46;
+
+let required = fuel * distance;
+
+if(required > 50){
+    console.log("Enough");
+}else{
+    console.log("Go On!!");
+}
