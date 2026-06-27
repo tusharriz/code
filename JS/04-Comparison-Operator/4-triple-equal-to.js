b75b3579a939a7728201270a3b31d2eedcd7fa05
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 19:57:26 +0530

    coding exercise

---
+++ JS/04-Comparison-Operator/4-triple-equal-to.js
@@ -0,0 +1,12 @@
+//triple equal to (===)
+// - true : It gives “true” only when the first value and data type are equal to the second value and data type.
+
+console.log("5" === "5") // true
+console.log(5 === 5) // true
+console.log("cat" === "cat") // true
+
+// - false : It gives “false” only when the first value and data type are equal to the second value and data type.
+
+console.log(5 === "5") // false
+console.log(5 === 6) // false
+console.log("cat" === "Cat") // false
