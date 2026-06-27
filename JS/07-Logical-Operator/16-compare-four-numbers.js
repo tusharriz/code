6af1f1509b9508077e2ddd9a0b2d57769f73bcf8
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 22:37:54 +0530

    coding exercise

---
+++ JS/07-Logical-Operator/16-compare-four-numbers.js
@@ -0,0 +1,19 @@
+`You are given four numbers stored in variables with the following names
+one, two, three, four
+If the value of both the following expression is true, print Yes , else print No
+1. one > two
+2. three > four
+
+Sample Input 1 
+1 2 4 3
+Sample Output 1
+No
+`
+
+let one = 1, two = 2, three = 3, four = 4;
+
+if(one > two && three > four){
+    console.log("Yes"); 
+}else{
+    console.log("No"); // No
+}
