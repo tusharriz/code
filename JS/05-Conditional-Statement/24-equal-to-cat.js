74e47782e6f6cc17ff69c683c073397528d6e376
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 20:57:34 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/24-equal-to-cat.js
@@ -0,0 +1,10 @@
+`You are given a string, stored in the variable with the name str
+Print Yes , if the string is equal to "cat", else print No`
+
+let str = "cat";
+
+if(str == "tom"){
+    console.log("Yes");
+}else{
+    console.log("No"); // No
+}
