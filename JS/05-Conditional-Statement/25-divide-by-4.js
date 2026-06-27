926be2c3787cc62486f99f60aaf38deeb4aec0d4
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 20:57:48 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/25-divide-by-4.js
@@ -0,0 +1,11 @@
+`You are given a number, stored in the variable with the name N
+Print Yes , if the number is divisible by 4, else print No
+Note : A number is divisible by 4, if the result of the following expression number % 4 == 0`
+
+let N = 12;
+
+if(N % 4 == 0){
+    console.log("Yes"); // Yes
+}else{
+    console.log("No");
+}
