401d3c15570b3486beef0531772af51da4087faa
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 20:54:36 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/14-check-food.js
@@ -0,0 +1,13 @@
+let rice_available = false;
+let wheat_available = true;
+let apple_available = true;
+
+if (rice_available) {
+    console.log("Buy rice");
+} else if (wheat_available) {
+     console.log("Buy Wheat"); // Buy Wheat
+} else if (apple_available) {
+     console.log("Buy apple");
+} else {
+     console.log("Nothing is available");
+}
