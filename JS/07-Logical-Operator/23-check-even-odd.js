c9ebb1bac963a8f4527e1d7ad6df7feaca6c7552
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 22:39:22 +0530

    coding exercise

---
+++ JS/07-Logical-Operator/23-check-even-odd.js
@@ -0,0 +1,16 @@
+`You are given a number, stored in the variable one
+If the number is even, then print Even , else print Odd
+
+Sample Input 1 
+4
+Sample Output 1
+Even
+`
+
+let one = 4;
+
+if(one%2==0){
+    console.log("Even"); // Even
+}else{
+    console.log("Odd");
+}
