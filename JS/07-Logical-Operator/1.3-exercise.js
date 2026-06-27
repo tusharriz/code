05089d0357bd8c34fe735cd0efe011171a5d7a0b
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 21:30:17 +0530

    coding exercise

---
+++ JS/07-Logical-Operator/1.3-exercise.js
@@ -0,0 +1,11 @@
+// First part: Without AND
+if (5 > 3) {
+    if (6 > 3) {
+        console.log("Both are true (without AND)"); // true
+    }
+}
+
+// Second part: With AND
+if (5 > 3 && 6 > 3) {
+    console.log("Both are true (with AND)"); // true
+}
