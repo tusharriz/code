c10c5a5911666eff19aea1df2c16853ea6eae142
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 20:59:22 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/30-even-odd.js
@@ -0,0 +1,11 @@
+`You are given a number, stored in the variable with the name N
+If the number is even, print Even , else print Odd
+If the number is divisible by 2, the number is considered Even , else the number is considered Odd`
+
+let N = 5;
+
+if(N%2==0){
+    console.log("Even");
+}else{
+    console.log("Odd"); // Odd
+}
