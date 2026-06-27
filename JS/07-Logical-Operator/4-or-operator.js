70afedbc7d1cca3bfada86c1c1f106128f5ea7ba
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 21:31:48 +0530

    coding exercise

---
+++ JS/07-Logical-Operator/4-or-operator.js
@@ -0,0 +1,7 @@
+// OR Operator (||)
+
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(true || true); // true
+console.log(false || false); // false
+console.log(true || true || true || false); // true
