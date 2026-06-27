21b11b63f92a8fbf5868a187874d5cb056fc8cf4
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 21:00:00 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/33-equal-5.js
@@ -0,0 +1,17 @@
+`You are given two numbers, stored in the variable with the name x and y
+Calculate the sum of x and y as sum = x + y
+if sum > 5 , Then print "Above 5"
+else If sum < 5 , then print "Below 5"
+else print "Equal 5"`
+
+let x = 2;
+let y = 3;
+let sum = x + y;
+
+if(sum > 5){
+    console.log("Above 5");
+}else if(sum < 5){
+    console.log("Below 5")
+}else{
+    console.log("Equal 5");
+}
