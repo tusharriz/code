aee24a835df49920c55c7d9a3a507da62f1ff04f
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 19:57:05 +0530

    coding exercise

---
+++ JS/04-Comparison-Operator/3-not-equal-to.js
@@ -0,0 +1,11 @@
+// not equal to (!=)
+// - true : It gives “true” only when the first value is not equal to the second value.
+
+console.log(5 != 4) // true => 5 is not equal to 4
+console.log("a" != "A") // true, since characters are case-sensitive
+
+
+// - false : It gives “false” only when the first value is equal to the second value.
+
+console.log(4 != 4) // false => it say's 4 is not equal to 4
+console.log("cat" != "cat") // false
