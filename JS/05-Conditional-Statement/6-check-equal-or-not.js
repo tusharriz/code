d550bd22f6c94bde50ac0092a9f3414f8313fc4d
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 20:52:26 +0530

    coding exercise

---
+++ JS/05-Conditional-Statement/6-check-equal-or-not.js
@@ -0,0 +1,19 @@
+// If the given two numbers are equal, print they are equal otherwise print no equal.
+
+let a=5;
+let b=5;
+
+if(a==b){
+  console.log("They are Equal"); // They are equal
+} else {
+  console.log("Not Equal");
+}
+
+let c=5;
+let d=15;
+
+if(c==d){
+  console.log("They are Equal"); 
+} else {
+  console.log("Not Equal"); // They are not equal
+}
