356264c692b533113c65bb883b54a7650b25681c
Tushar Riz <er.tusharriz@gmail.com>
2026-06-27 21:25:50 +0530

    coding exercise

---
+++ JS/06-Evaluation/3-problem.js
@@ -0,0 +1,28 @@
+`Problem 3: Given the following values of first_name, last_name, x and y. Get the following output using concatenation only.
+
+Given Input
+let first_name="Clark";
+let last_name="Kent";
+let x="Aman";
+let y="Singh";
+
+Sample Output
+Clark - Kent
+Aman
+$
+Singh
+$
+
+Keep in mind you have to get the output using a single console.log statemnet using concatenation only. */`
+
+let first_name = "Clark";
+let last_name = "Kent";
+let x = "Aman";
+let y = "Singh";
+
+console.log(first_name + " " + "-" + " " + last_name + "\n" + x + "\n" + "$" + "\n" + y + "\n" +"$");
+//Clark - Kent
+// Aman
+// $
+// Singh
+// $
