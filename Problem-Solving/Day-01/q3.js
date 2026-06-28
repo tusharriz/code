4c5877077b2d5bd23896a61f427805b2ec83862e
Tushar Riz <er.tusharriz@gmail.com>
2026-06-28 23:42:39 +0530

    coding exercise

---
+++ Problem-Solving/Day-01/q3.js
@@ -0,0 +1,32 @@
+// Q1.
+let x = 7;
+if(x % 2 != 0){
+    console.log("Where");
+    if(x > 4){
+        console.log("Here");
+    }
+    else{
+        console.log("There");
+    }
+}
+else{
+    console.log("Nowhere");
+}
+// Output: Where
+// Output: Here
+
+// Q2.
+let x = 4;
+if(x % 2 != 0){
+    console.log("Where");
+    if(x > 4){
+        console.log("Here");
+    }
+    else{ 
+        console.log("There");
+    }
+}
+else{
+    console.log("Nowhere");
+}
+// Output: Nowhere
