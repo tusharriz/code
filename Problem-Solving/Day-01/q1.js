b19d743dded192d4d268e1bc25fdc1f36e2d3b5e
Tushar Riz <er.tusharriz@gmail.com>
2026-06-28 23:43:11 +0530

    codind exercise

--- Problem-Solving/Day-01/q1.js
+++ Problem-Solving/Day-01/q1.js
@@ -1,2 +1,18 @@
 let x = (4 > 5) || (! (3 != 3))
 console.log(x); //  True
+
+let x = (4 > 5) && (! (3 != 3))
+console.log(x); //  false
+
+let a = !(5 != 5) || ( 2 >= 2 );
+console.log(a); // true
+
+let b = !(5 != 5) && ( 2 >= 2 );
+console.log(b); // true
+
+let b = !(5 != 5) && ( 2 > 2 );
+console.log(b); // false
+
+let b = !(5 != 5) || ( 2 > 2 );
+console.log(b); // true
+
