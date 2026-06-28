5111124bbba5196f7a489d4a7342f29c1dc06219
Tushar Riz <er.tusharriz@gmail.com>
2026-06-28 23:42:07 +0530

    coding exercise

---
+++ Problem-Solving/Day-01/q2.js
@@ -0,0 +1,90 @@
+// Q1.
+let x = 15;
+
+if(x == 15){
+    console.log("Ram");
+}
+else if(x % 5 == 0){
+    console.log("Shayam");
+}
+else{
+    console.log("Hari");
+}
+// output : Ram
+
+
+// Q2.
+let x = 15;
+
+if(x == 16){
+    console.log("Ram");
+}
+else if(x % 5 == 0){
+    console.log("Shayam");
+}
+else{
+    console.log("Hari");
+}
+// output : Shayam
+
+
+// Q3.
+let x = 15;
+
+if(x == 16){
+    console.log("Ram");
+}
+else if(x % 5 != 0){
+    console.log("Shayam");
+}
+else{
+    console.log("Hari");
+}
+// output : Hari
+
+
+// Q4.
+let x = 15;
+
+if(x == 16){
+    console.log("Ram");
+}
+if(x % 5 == 0){
+    console.log("Shayam");
+}
+else{
+    console.log("Hari");
+}
+// output : Shayam
+
+
+// Q5.
+let x = 15;
+
+if(x == 15){
+    console.log("Ram");
+}
+if(x % 5 == 0){
+    console.log("Shayam");
+}
+else{
+    console.log("Hari");
+}
+// output : Ram
+// output : Shayam
+
+
+// Q6.
+let x = 16;
+
+if(x == 15){
+    console.log("Ram");
+}
+if(x % 5 == 0){
+    console.log("Shayam");
+}
+else{
+    console.log("Hari");
+}
+
+// output : Hari
