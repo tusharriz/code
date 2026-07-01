9a8cab38700063cfdceb598e5931d34516c44aad
Tushar Riz <er.tusharriz@gmail.com>
2026-07-01 12:14:12 +0530

    coding exercise

---
+++ JS/08-Increment-Decrement/1-increment-decrement.js
@@ -0,0 +1,21 @@
+//Note : Both pre increment ++i and post increment i++ are the same and both pre decrement --i and post decrement i-- are the same. 
+
+//Increment:-
+
+let x = 10;
+x = x + 1; //increment by 1
+console.log(x); //11
+
+let y = 25;
+y = y + 10; //increment by 10
+console.log(y); //35
+
+// Decrement:-
+
+let a = 10;
+a = a - 1; //decrement by 1
+console.log(a); //9 
+
+let b = 20;
+b = b - 10; //decrement by 10
+console.log(b); //10
