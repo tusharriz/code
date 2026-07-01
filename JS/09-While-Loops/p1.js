e9f4e3641c9626db37fec10b495cc4af0782cfa1
Tushar Riz <er.tusharriz@gmail.com>
2026-07-01 13:10:46 +0530

    coding exercise

---
+++ JS/09-While-Loops/p1.js
@@ -0,0 +1,10 @@
+// Problem 1: Print the numbers from the given starting point till ending point (including both start and end)
+
+let startPoint = 1;
+let endPoint = 5;
+
+let i = startPoint;
+while(i<=endPoint){
+    console.log(i);
+    i++;
+}
