0286bb437fcbf807212c838085ae965620028a98
Tushar Riz <er.tusharriz@gmail.com>
2026-07-01 13:01:07 +0530

    coding exercise

---
+++ JS/09-While-Loops/4-print-your-name-10-times.js
@@ -0,0 +1,20 @@
+// print-your-name-10-times
+
+let i=1;
+
+while(i<=10){
+    console.log(i+".","Satnaam");
+    i++;
+}
+
+// output:-
+// 1. Satnaam
+// 2. Satnaam
+// 3. Satnaam
+// 4. Satnaam
+// 5. Satnaam
+// 6. Satnaam
+// 7. Satnaam
+// 8. Satnaam
+// 9. Satnaam
+// 10. Satnaam
