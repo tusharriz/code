e13fc59bab0bac653aba443d95c9810c3d404900
Tushar Riz <er.tusharriz@gmail.com>
2026-07-01 12:15:47 +0530

    coding exercise

---
+++ JS/08-Increment-Decrement/5-pre-post-decrement.js
@@ -0,0 +1,15 @@
+//pre-decrement
+
+let x = 10;
+let y = --x;
+
+console.log(y); // 9
+console.log(x); // 9
+
+//post-decrement
+
+let a = 15;
+let b = a--;
+
+console.log(b); //15
+console.log(a); //14
