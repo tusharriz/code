a366b00d301f6ebcd744668e9995a0eede828beb
Tushar Riz <er.tusharriz@gmail.com>
2026-07-01 13:02:25 +0530

    coding exercise

---
+++ JS/09-While-Loops/6-break.js
@@ -0,0 +1,21 @@
+// break : It will break the loop and go out of it.
+
+// for e.g.
+
+// print 1 to 10 but if i === 5, break the loop
+
+let i=1;
+
+while(i<=10){
+    if(i===5){
+        break;
+    }
+    console.log(i);
+    i++;
+}
+
+// output:-
+// 1
+// 2
+// 3
+// 4
