deea1a58c5568d4afa77a02dbc8f18c3acecdc27
Tushar Riz <er.tusharriz@gmail.com>
2026-07-01 13:00:51 +0530

    coding exercise

---
+++ JS/09-While-Loops/3-print-the-counting-1-to-10.js
@@ -0,0 +1,24 @@
+// Print the counting from 1 to 10
+
+//where to start? 
+//where to end?
+//inc/dec? and by what value?
+
+let i=1;
+
+while(i<=10){
+    console.log(i);
+    i++;
+}
+
+// output
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
