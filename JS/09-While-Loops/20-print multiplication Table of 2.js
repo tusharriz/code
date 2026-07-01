641efd355ac397a09749b96f90af42b3c13d6b1f
Tushar Riz <er.tusharriz@gmail.com>
2026-07-01 13:08:17 +0530

    coding exercise

---
+++ JS/09-While-Loops/20-print multiplication Table of 2.js
@@ -0,0 +1,20 @@
+//print multiplication Table of 2
+
+let i=1;
+while(i<=20){
+    if(i%2===0){
+        console.log(i);
+    }
+    i++;
+}
+
+//or
+
+{
+    let num = 2;
+    let i = 1;
+    while(i<=10){
+        console.log(num * i);
+        i++;
+    }
+}
