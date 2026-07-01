939b5d4bff925249152901b879cc0f392b67497f
Tushar Riz <er.tusharriz@gmail.com>
2026-07-01 13:10:01 +0530

    coding exercise

---
+++ JS/09-While-Loops/26-Even Sum Below N.js
@@ -0,0 +1,24 @@
+// Even Sum Below N
+
+`You are given a number, stored in a variable with the name num
+Find the sum of all even numbers greater than zero, and less than or equal to the value stored in num
+For example, if the value stored in num = 5 , then all the even numbers smaller than or equal to 5, and greater than zero are
+2
+4
+Therefore, the sum becomes, sum = 2 + 4 = 6 , which is the required output
+
+Sample Input 1 
+6
+Sample Output 1
+12`
+
+let num = 6;
+let sum = 0;
+let i = 1;
+while(i<=num){
+    if(i%2==0){
+        sum = sum + i;
+    }
+    i++;
+}
+console.log(sum); // 12
