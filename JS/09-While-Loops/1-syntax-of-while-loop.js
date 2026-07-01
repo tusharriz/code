57ad88608ad5934b04bd6fca0f85cb3d529dda70
Tushar Riz <er.tusharriz@gmail.com>
2026-07-01 13:00:37 +0530

    coding exercise

---
+++ JS/09-While-Loops/1-syntax-of-while-loop.js
@@ -0,0 +1,17 @@
+// Syntax of while loop
+
+// Variable Intialization
+// while (condition) {
+    // operation/code/task
+    // increment/decrement
+// }
+
+
+// for example : print counting from 1 to 5.
+
+let i=1;            // variable intialization
+while(i<=5){        // condition
+    console.log(i); // operation/code/task
+    i++; // i=i+1   // increment
+}
+
---
+++ JS/09-While-Loops/2-print-hello-world-five-times.js
@@ -0,0 +1,19 @@
+// print-hello-world-five-times
+
+//where to start? => 1
+//where to end? => 5
+//inc/dec? and by what value? =>inc by 1
+
+let i=1;
+
+while(i<=5){
+    console.log("Hello World");
+    i++;// i=i+1;
+}
+
+//output:-
+//Hello World
+//Hello World
+//Hello World
+//Hello World
+//Hello World
