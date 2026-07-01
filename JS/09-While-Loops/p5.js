86bde0d9191ff37ac7593442600a36031cbd65af
Tushar Riz <er.tusharriz@gmail.com>
2026-07-01 13:11:55 +0530

    coding exercise

---
+++ JS/09-While-Loops/p5.js
@@ -0,0 +1,25 @@
+// print the number from staring to ending including both start and end;
+{
+    let start = 15;
+    let end = 45;
+
+    while (start <= end) {
+        console.log(start);
+        start++;
+    }
+}
+
+// OR
+
+{
+    let start = 5;
+    let i = start;
+    let end = 10;
+
+    while (i <= end) {
+        console.log(i);
+        i++;
+    }
+}
+
+
