976903be869a11ca0b1bcf508095ef7cebb52ceb
Tushar Riz <er.tusharriz@gmail.com>
2026-07-02 15:01:06 +0530

    coding exercise

---
+++ JS/10-For-Loops/10-print-average-of-odd.js
@@ -0,0 +1,17 @@
+// calculate the average of odd numbers between 1 and 100 inclusig both
+//Avg=sum/count
+
+let sum = 0;
+let count = 0;
+
+for(let i=1; i<=100; i++){
+    if(i%2!==0){
+        sum = sum + i;
+        count++;
+    }
+}
+// console.log(sum) // 2500
+// console.log(count); // 50
+
+let average = sum/count;
+console.log(average); // 50
