54448fbf01feb1e6733e38ea6a79f77bb06f72ed
Tushar Riz <er.tusharriz@gmail.com>
2026-07-04 13:31:26 +0530

    coding exercise

---
+++ JS/11-Nested-Loops/03-farm-1-seed-1.js
@@ -0,0 +1,16 @@
+//father son duo==> Plant 1 seed in farm 1, plant 2 seeds in farm 2 and 3 seeds in farm 3 .......
+
+
+for(let i=1; i<=5; i++){
+    let bag = "";
+    for(let j=1; j<=i; j++){
+        bag = bag + "*";
+    }
+    console.log(bag);
+}
+
+// *
+// **
+// ***
+// ****
+// *****
