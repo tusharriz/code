046be3cdc2a814fdbd63631a60b2184a67a9dde1
Tushar Riz <er.tusharriz@gmail.com>
2026-07-04 13:26:39 +0530

    coding exercise

---
+++ JS/11-Nested-Loops/0.4-print-the-output.js
@@ -0,0 +1,21 @@
+// print the output;
+
+// 12345
+// 1234
+// 123
+// 12
+// 1
+
+for(let i=5; i>=1; i--){
+    let bag = "";
+    for(let j=1; j<=i; j++){
+        bag = bag + j + " "
+    }
+    console.log(bag);
+}
+
+// 1 2 3 4 5 
+// 1 2 3 4 
+// 1 2 3 
+// 1 2 
+// 1
