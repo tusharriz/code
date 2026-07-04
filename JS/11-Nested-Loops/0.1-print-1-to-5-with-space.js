afe4dd4f762bbd09785274b5b48d1092ff2872b8
Tushar Riz <er.tusharriz@gmail.com>
2026-07-04 13:24:44 +0530

    coding exercise

---
+++ JS/11-Nested-Loops/0.1-print-1-to-5-with-space.js
@@ -0,0 +1,23 @@
+// print 1 to 5 in the same line for 5 times in the new line also.
+
+// Output:
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+
+for(let i=1; i<=5; i++){
+    let bag = "";
+    for(let j=1; j<=5; j++){
+        bag = bag + j + " ";
+    }
+    console.log(bag);
+    
+}
+
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
