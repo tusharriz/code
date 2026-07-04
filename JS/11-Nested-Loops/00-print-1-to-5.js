73f01f92d713b0cd06fb1c72e9b08d84f9d96761
Tushar Riz <er.tusharriz@gmail.com>
2026-07-04 13:23:46 +0530

    coding exercise

---
+++ JS/11-Nested-Loops/00-print-1-to-5.js
@@ -0,0 +1,22 @@
+// print 1 to 5 in the same line for 5 times in the new line also.
+
+// Output:
+// 12345
+// 12345
+// 12345
+// 12345
+// 12345
+
+for(let i=1; i<=5; i++){
+    let bag = "";
+    for(let j=1; j<=5; j++){
+        bag = bag + j;
+    }
+    console.log(bag);
+}
+
+// 12345
+// 12345
+// 12345
+// 12345
+// 12345
