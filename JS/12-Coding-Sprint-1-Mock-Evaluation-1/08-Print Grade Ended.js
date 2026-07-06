b842bb7f3acf93b275271b7c01d905ecc271bce7
Tushar Riz <er.tusharriz@gmail.com>
2026-07-06 23:49:08 +0530

    coding exercise

---
+++ JS/12-Coding-Sprint-1-Mock-Evaluation-1/08-Print Grade Ended.js
@@ -0,0 +1,30 @@
+// Print Grade Ended
+
+`You are given a number, stored in the variable with the name total
+If the following expression is true
+total == 100, print "A", without quotes
+Else if the following expression is true,
+total >= 90, print "B", without quotes
+Else if the following expression is true,
+total >= 80, print "C", without quotes
+Else, print "F", without quotes
+
+Sample Input 1 
+80
+Sample Output 1
+C
+`
+
+let total = 80;
+
+if(total == 100){
+    console.log("A");
+}else if(total >= 90){
+    console.log("B");
+}else if(total >= 80){
+    console.log("C");
+}else{
+    console.log("F");
+}
+
+// C
