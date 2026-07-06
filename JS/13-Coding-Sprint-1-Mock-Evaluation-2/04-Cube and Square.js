e79c201a36a8c8538d0b87b9fee9ef3f5aa28f74
Tushar Riz <er.tusharriz@gmail.com>
2026-07-06 23:53:14 +0530

    coding exercise

---
+++ JS/13-Coding-Sprint-1-Mock-Evaluation-2/04-Cube and Square.js
@@ -0,0 +1,15 @@
+// Cube and Square
+
+`You are given two numbers n and m, you need to check if cube of n is greater than square of m.
+
+Sample Input 1 
+2 3
+Sample Output 1
+false`
+
+let n = 2;
+let m = 3;
+
+console.log((n*n*n) > (m*m));
+
+// false
