af954f637f595d860f91d12ba792725c1d091bac
Tushar Riz <er.tusharriz@gmail.com>
2026-07-06 23:51:25 +0530

    codind exercise

---
+++ JS/14-Coding-C1-Evaluation/01-Operation on Numbers I.js
@@ -0,0 +1,28 @@
+// Operation on Numbers I 
+
+`You are given a number stored in a variable with the name N
+You have to do the following operations on the number stored in a variable with the name N
+1. Multiply the number with 10
+2. Add 2 to the number
+3. Subtract 11 from the number
+Print the result obtained after the three operations
+For example, consider the value stored in N = 5 . So, the result of the 3 operations with the following numbers
+Step 1 -> 5*10 = 50
+Step 2 -> 50 + 2 = 52
+Step 3 -> 52 - 11 = 41
+Therefore, the final output of the number is 41
+
+Sample Input 1 
+5
+Sample Output 1
+41`
+
+let n = 5;
+
+n = n * 10;
+n = n + 2;
+n = n - 11;
+
+console.log(n);
+
+// 41
