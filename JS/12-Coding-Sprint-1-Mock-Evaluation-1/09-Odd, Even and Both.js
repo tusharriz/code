3b86377b41eac8b303604f8617ca2b5572a54636
Tushar Riz <er.tusharriz@gmail.com>
2026-07-06 23:49:34 +0530

    coding exercise

---
+++ JS/12-Coding-Sprint-1-Mock-Evaluation-1/09-Odd, Even and Both.js
@@ -0,0 +1,26 @@
+// Odd, Even and Both
+
+`You are given two numbers, stored in the variable with the following names
+one, two
+If both the values are even, print Even
+Else if both the values are odd, print Odd
+else print Even-Odd
+
+Sample Input 1 
+3 6
+Sample Output 1
+Even-Odd
+`
+
+let one = 3;
+let two = 6;
+
+if(one%2==0 && two%2==0){
+    console.log("Even");
+}else if(one%2!=0 && two%2!=0){
+    console.log("Odd");
+}else{
+    console.log("Even-Odd");
+}
+
+// Even-Odd
