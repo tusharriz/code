2f7e0bc81f852989c3b2b73d938879c2c92eab9b
Tushar Riz <er.tusharriz@gmail.com>
2026-07-06 23:52:01 +0530

    codind exercise

---
+++ JS/14-Coding-C1-Evaluation/02-Check Greater.js
@@ -0,0 +1,23 @@
+// Check Greater 
+
+`You are given four numbers stored in variables with the name, A, B, C and D
+Print the result of the following operation
+A is greater than B and C is greater than D
+
+Sample Input 1 
+26 20 30 28
+Sample Output 1
+true`
+
+let a = 26;
+let b = 20;
+let c = 30;
+let d = 28;
+
+if(a > b && c > d){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+// ture
