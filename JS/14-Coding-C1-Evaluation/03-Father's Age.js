08e1a4cf1e7114095a2159df70c29c2fabccd3e4
Tushar Riz <er.tusharriz@gmail.com>
2026-07-06 23:52:50 +0530

    codind exercise

---
+++ JS/14-Coding-C1-Evaluation/03-Father's Age.js
@@ -0,0 +1,16 @@
+// Father's Age
+
+`Anna is X years old. Her Father is 20 years older than twice of Anna's age. Find her father's age.
+
+Sample Input 1 
+6
+Sample Output 1
+32
+`
+
+let x = (6 * 2);
+let fatherAge = 20;
+
+console.log(x + fatherAge);
+
+// 32
