15cff270c297289b18b41acdfc2dc62e34f4da90
Tushar Riz <er.tusharriz@gmail.com>
2026-07-09 21:32:01 +0530

    coding exercise

---
+++ JS/16-Strings/05-add-at-the-end.js
@@ -0,0 +1,10 @@
+// How to add something at the end?
+//push and pop are not going to work.
+
+//concatenation
+
+let name1="rober";
+
+//add "t" at the end
+
+console.log(name1+"t"); //concatenation robert
