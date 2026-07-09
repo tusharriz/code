da3e497c2a648a7581fe80e828b70095e4802b9e
Tushar Riz <er.tusharriz@gmail.com>
2026-07-09 20:14:30 +0530

    coding exercise

--- JS/16-Strings/02-how-to-print-h.js
+++ JS/16-Strings/02-how-to-print-h.js
@@ -2,3 +2,12 @@
 
 let str = "Coding School";
 console.log(str[9]); // h
+
+// access-with-.length-property
+console.log(str.length-1); // 12
+console.log(str.length-2); // 11
+
+console.log(str[str.length-1]); // l
+console.log(str[str.length-2]); // 0
+console.log(str[str.length-3]); // 0
+console.log(str[str.length-4]); // h
