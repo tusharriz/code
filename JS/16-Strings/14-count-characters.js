dc4263788e51a6903272f2af224a47df73746752
Tushar Riz <er.tusharriz@gmail.com>
2026-07-09 23:25:48 +0530

    coding exercise

---
+++ JS/16-Strings/14-count-characters.js
@@ -0,0 +1,19 @@
+// Given an array of string count the overall total number of characters
+
+let arr = ["html", "css", "javascript"];
+
+//count of elements in array
+
+// console.log(arr.length); // 3 
+// console.log(arr[0].length); // 4
+// console.log(arr[1].length); // 3
+
+
+//count total numbers of characters of the strings
+
+let count = 0;
+
+for(let i=0; i<=arr.length-1; i++){
+    count = count + arr[i].length;
+}
+console.log(count); // 17
