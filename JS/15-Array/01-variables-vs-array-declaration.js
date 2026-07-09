6b7bd2c841599f00887964905f919f89daffc4f5
Tushar Riz <er.tusharriz@gmail.com>
2026-07-09 19:16:27 +0530

    coding exercise

---
+++ JS/15-Array/01-variables-vs-array-declaration.js
@@ -0,0 +1,20 @@
+// 01-variables-vs-array-declaration
+
+//declare a variable
+let emp1="Clark";
+let emp2="Barry";
+let emp3="Bruce";
+let emp4="Tony"; 
+
+//vs
+
+//declare an array
+let empNames = ["Clark", "Barry", "Bruce", "Tony"];
+console.log(empNames);
+// [ 'Clark', 'Barry', 'Bruce', 'Tony' ]
+
+// we can store any kind of data type in array;
+
+let array = ["Ram", 1598, true, 0.717];
+console.log(array);
+// [ 'Ram', 1598, true, 0.717 ]
