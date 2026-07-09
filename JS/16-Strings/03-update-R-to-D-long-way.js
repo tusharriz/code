8fb4b44b820d193df3a0a977567547027eaca603
Tushar Riz <er.tusharriz@gmail.com>
2026-07-09 21:27:22 +0530

    coding exercise

---
+++ JS/16-Strings/03-update-R-to-D-long-way.js
@@ -0,0 +1,48 @@
+// update Ronald to Donald;
+
+// long method
+let str = "Ronald";
+let arr = [];
+
+//convert the string into array
+for (let i = 0; i <= str.length - 1; i++) {
+  arr.push(str[i]);
+}
+// console.log(arr); // [ 'R', 'o', 'n', 'a', 'l', 'd' ]
+// console.log(arr[0]); // R
+
+//update "R" to "D"
+arr[0] = "D";
+console.log(arr); // [ 'D', 'o', 'n', 'a', 'l', 'd' ]
+
+//convert the array into string
+let bag = "";
+for (let j = 0; j <= arr.length - 1; j++) {
+  bag = bag + arr[j];
+}
+
+console.log(bag); //Donald
+
+
+
+{
+  let name = "Ronald";
+  let arr = [];
+  let nstr = "";
+  for (let i = 0; i <= name.length - 1; i++) {
+    arr.push(name[i]);
+  }
+  console.log(arr); // [ 'R', 'o', 'n', 'a', 'l', 'd' ]
+
+  arr[0] = "D";
+
+  console.log(arr); // [ 'D', 'o', 'n', 'a', 'l', 'd' ]
+
+  for(let i=0; i<=arr.length-1; i++){
+    nstr = nstr + arr[i];
+  }
+  console.log(nstr); // Donald
+
+
+}
+
