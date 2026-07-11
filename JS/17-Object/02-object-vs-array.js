f548561dc7ff14e7f0e605e2613dbc22b52cbe4a
Tushar Riz <er.tusharriz@gmail.com>
2026-07-11 23:30:14 +0530

    codind exercise

---
+++ JS/17-Object/02-object-vs-array.js
@@ -0,0 +1,23 @@
+// Object vs Array
+
+//  declaring an array
+let arr  = ["Ram", 26, "male", "Delhi", "coding"];
+console.log(arr[3]); // Delhi
+
+
+// declaring an object
+
+let obj = {
+    name : "Ram",
+    age : 26,
+    gender : "male",
+    city : "Delhi",
+    hobbies : "coding"
+};
+
+console.log(obj["name"]); // Ram
+console.log(obj["age"]); // 26
+console.log(obj["gender"]); // male
+console.log(obj["city"]); // Delhi
+console.log(obj["hobbies"]); // coding
+
