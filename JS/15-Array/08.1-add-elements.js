356d0e409c0c395633f3c2d72aa33e09d367d8aa
Tushar Riz <er.tusharriz@gmail.com>
2026-07-11 19:56:58 +0530

    coding exercise

---
+++ JS/15-Array/08.1-add-elements.js
@@ -0,0 +1,10 @@
+//Add Element;
+//With the help of "PUSH" method you can easily add the element at the "END" of an array;
+
+let names = ["Om","Hari","Ram","Prasad","Krishna"];
+console.log(names); 
+console.log(names.length); //5
+
+names.push("Hari");
+console.log(names);
+console.log(names.length); // intially the length was 5, but after add one more element to it the lenght is 6.
