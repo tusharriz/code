9135dc0b318c4449c2be1caffbc44340fa54aad2
Tushar Riz <er.tusharriz@gmail.com>
2026-07-12 00:11:31 +0530

    coding exercise

---
+++ JS/17-Object/07-print-in-new-line.js
@@ -0,0 +1,17 @@
+//print the hobbies in new line
+//Coding
+//Graphic Novels
+//Movies
+
+let details={
+    name: "Chunnu",
+    age:20,
+    location: "Manali",
+    hobbies: ["Coding", "Graphic Novels", "Movies"] //array
+};
+
+console.log(details["hobbies"][0]); // Coding
+console.log(details["hobbies"][1]); // Graphic Novels
+console.log(details["hobbies"][2]); // Movies
+
+console.log(details["hobbies"].length); // 3
