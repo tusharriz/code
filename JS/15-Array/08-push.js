acadfac47cb8f1838cb2b54f2cb252589bb520c5
Tushar Riz <er.tusharriz@gmail.com>
2026-07-11 19:56:44 +0530

    coding exercise

---
+++ JS/15-Array/08-push.js
@@ -0,0 +1,18 @@
+// push method
+
+// we have a method called as "push".
+// push will add the new element always at the end of the array.
+
+let veg=["Potato", "Tomato", "Egg Plant", "Cucumber"];
+console.log(veg);
+console.log(veg.length);
+
+veg.push("Carrot");
+console.log(veg);
+console.log(veg.length);
+veg.push(" ");
+console.log(veg);
+
+// NOTE : hum push sirf array mei hi kr skte hai, chahe empty array ho ya already elements present ho array mei.
+
+// string mei kbhi push nahi kr skte because push array ka method hai toh array ke saat hi work karega
