be3c39384703b1494414be612e3b2b6667061a7d
Tushar Riz <er.tusharriz@gmail.com>
2026-07-11 20:27:38 +0530

    coding exercise

---
+++ JS/15-Array/23.js
@@ -0,0 +1,11 @@
+// declare an array, put the names of any 4 vegetables and priont each vegetable in separate line.
+
+let veg=["Potato", "Tomato", "Egg Plant", "Cucumber"];
+
+// console.log(veg[0]);
+// console.log(veg[1]);
+// console.log(veg[2]);
+
+console.log(veg[3]); // it gives element => Cucumber 
+console.log(veg.length-1); // it gives index => 3
+console.log(veg[veg.length-1]); // it gives element => Cucumber
