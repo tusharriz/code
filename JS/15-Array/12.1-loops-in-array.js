7b9a57d25070ac63aab418902d835e2e90d7ffde
Tushar Riz <er.tusharriz@gmail.com>
2026-07-11 20:00:03 +0530

    coding exercise

---
+++ JS/15-Array/12.1-loops-in-array.js
@@ -0,0 +1,18 @@
+// loops in array
+//how to print all elements of the veg array;
+
+let veg=["Potato", "Tomato", "Egg Plant", "Cucumber"];
+
+//everything is same except the index value
+//0-->last index, getting increased by 1, index++
+
+//using loops in array
+
+for(let i=0; i<=veg.length-1; i++){
+    console.log(veg[i]);
+}
+
+// Potato                              
+// Tomato                              
+// Egg Plant                           
+// Cucumber
