b06c74be976afc798700fce7db88132d67ba45d4
Tushar Riz <er.tusharriz@gmail.com>
2026-07-11 20:48:35 +0530

    coding exercise

---
+++ JS/15-Array/41.js
@@ -0,0 +1,6 @@
+let names = ["Om","Hari","Ram","Prasad","Krishna"];
+
+// console.log(names[4-1]);// names[4-1] == names[3], answer is Prasad 
+// console.log(names[3]-(names.length-1))
+
+console.log(names[5]);
