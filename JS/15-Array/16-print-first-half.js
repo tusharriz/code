208bd7f18e9f4b945b78a66caa3d9075ac1b74eb
Tushar Riz <er.tusharriz@gmail.com>
2026-07-11 20:05:10 +0530

    coding exercise

---
+++ JS/15-Array/16-print-first-half.js
@@ -0,0 +1,15 @@
+//print the first half of the array
+
+let arr=[1, 4, 5, 7, 8, 33, 88, 99];
+
+//print first half
+for(let i=0; i<arr.length/2; i++){
+    console.log(arr[i]);
+}
+
+// 1
+// 4
+// 5
+// 7
+
+//In this case if the length of the array is not even then also it will print the first half.
