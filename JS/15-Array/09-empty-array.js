547ff288e4db04f74540d82f6c08fc60b08dc893
Tushar Riz <er.tusharriz@gmail.com>
2026-07-11 19:57:43 +0530

    coding exercise

---
+++ JS/15-Array/09-empty-array.js
@@ -0,0 +1,12 @@
+//empty array
+
+let arr=[]; //empty array
+
+arr.push("Batman");
+arr.push("Superman");
+arr.push("Flash", "Green Lantern");
+
+console.log(arr.length); // 4
+console.log(arr); 
+
+console.log(arr[arr.length-8]); // undefined
