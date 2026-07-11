01cfcee2847a58931f7a013b7036761f7da70fb0
Tushar Riz <er.tusharriz@gmail.com>
2026-07-11 20:52:46 +0530

    coding exercise

---
+++ JS/15-Array/42-print-position.js
@@ -0,0 +1,13 @@
+// Given an array print the position (starting with 1) and the element in a single line.
+
+let arr = ["ram","shyam","chandan","komal","nirmal"];
+
+for(let i=0; i<=arr.length-1; i++){
+    console.log(i+1+".", arr[i]);
+}
+
+// 1. ram
+// 2. shyam
+// 3. chandan
+// 4. komal
+// 5. nirmal
