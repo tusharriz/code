5f91e8853c26ffa87fdd3c17796024d0393723db
Tushar Riz <er.tusharriz@gmail.com>
2026-07-12 00:10:12 +0530

    coding exercise

---
+++ JS/17-Object/06-print-hobbies-only.js
@@ -0,0 +1,11 @@
+//how to print hobbies
+
+let details={
+    name: "Chunnu",
+    age:20,
+    location: "Manali",
+    hobbies: ["Coding", "Graphic Novels", "Movies"] //array
+};
+
+console.log(details["hobbies"]);
+//[ 'Coding', 'Graphic Novels', 'Movies'
