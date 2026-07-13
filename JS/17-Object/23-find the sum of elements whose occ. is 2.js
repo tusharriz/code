3dc19e80e3347795d69c160604a8f351d164619b
Tushar Riz <er.tusharriz@gmail.com>
2026-07-13 12:31:22 +0530

    coding exercise

---
+++ JS/17-Object/23-find the sum of elements whose occ. is 2.js
@@ -0,0 +1,49 @@
+//find the sum of elements whose occ. is eual to 2 times
+
+let arr=[7,2,7,3,4,4,2];
+let obj = {};
+
+for(let i=0; i<=arr.length-1; i++){
+    if(obj[arr[i]] == undefined){
+        obj[arr[i]] = 1;
+    }else{
+        obj[arr[i]]++;
+    }
+}
+
+// console.log(obj); // { '2': 2, '3': 1, '4': 2, '7': 2 }
+
+let sum = 0;
+
+for(let key in obj){
+    if(obj[key] == 2){
+        sum = sum + Number(key);
+    }
+}
+console.log(sum); // 
+
+{
+    let arr=[7,2,7,7,3,4,4,2];
+    let obj = {};
+
+    for(let i=0; i<=arr.length-1; i++){
+        let key = arr[i];
+        if(obj[key] == undefined){
+            obj[key] = 1;
+        }else{
+            obj[key]++;
+        }
+        
+    }
+    console.log(obj); // { '2': 2, '3': 1, '4': 2, '7': 3 }
+
+    let sum = 0;
+    for(let key in obj){
+        // console.log(obj[key]); { '2': 2, '3': 1, '4': 2, '7': 3 }
+        if(obj[key] == 2){
+            sum = sum + (+key);
+        }
+    }
+    console.log(sum); // 6
+}
+
