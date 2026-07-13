e06bcb854b83874438acdc9e2acb6adda2c213a0
Tushar Riz <er.tusharriz@gmail.com>
2026-07-13 12:31:50 +0530

    coding exercise

---
+++ JS/17-Object/24-Find the element whose occ. is 1.js
@@ -0,0 +1,45 @@
+// Find the element whose occ. is 1
+
+let arr= [2,7,7,3,4,2];
+
+let obj = {};
+
+for(let i=0; i<=arr.length-1; i++){
+    if(obj[arr[i]] == undefined){
+        obj[arr[i]] = 1;
+    }else{
+        obj[arr[i]]++;
+    }
+}
+
+// console.log(obj); // { '2': 2, '3': 2, '4': 1, '7': 2 }
+
+for(let key in obj){
+    if(obj[key] == 1){
+        console.log(key);
+    }
+}
+
+// 3
+// 4
+
+{
+    let arr= [3,2,7,7,3,4,2];
+    let obj = {};
+    
+    for(let i=0; i<=arr.length-1; i++){
+        let key = arr[i];
+        if(obj[key] == undefined){
+            obj[key] = 1;
+        }else{
+            obj[key]++;
+        }
+    }
+    // console.log(obj); // { '2': 2, '3': 2, '4': 1, '7': 2 }
+
+    for(let key in obj){
+        if(obj[key] == 1){
+            console.log(key); // 4
+        }
+    }
+}
