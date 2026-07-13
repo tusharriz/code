860af1a149c0b96b571d7bbf1afbb93e4daac138
Tushar Riz <er.tusharriz@gmail.com>
2026-07-13 18:28:20 +0530

    coding exercise

---
+++ JS/17-Object/q9.js
@@ -0,0 +1,22 @@
+let emp_data=[
+    {name:"Ajay", age:45, salary:25000},
+    {name:"Chunnu", age:24, salary:35000},
+    {name:"Aman", age:45, salary:20000},
+    {name:"Varun", age:45, salary:45000}
+  ];
+
+  let emp_detail = "Pulkit";
+  let emp_age = 25;
+  let salary = 50000;
+//   console.log(emp_data);
+
+  for(let i=0; i<=emp_data.length-1; i++){
+    let emp_data = {};
+    // console.log(obj)
+    // delete obj["salary"];
+    emp_data["name"]=emp_name;
+    emp_data["age"]=emp_age
+    emp_data["name"]="salary"
+     
+  }
+   
