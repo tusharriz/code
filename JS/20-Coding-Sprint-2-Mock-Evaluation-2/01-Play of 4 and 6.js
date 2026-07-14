efee356e204c01f6d600973ab71e392fad659ead
Tushar Riz <er.tusharriz@gmail.com>
2026-07-14 20:29:52 +0530

    coding exercise

---
+++ JS/20-Coding-Sprint-2-Mock-Evaluation-2/01-Play of 4 and 6.js
@@ -0,0 +1,16 @@
+// Play of 4 and 6
+
+function playOf4and6(N) {
+    // Write code here
+    
+    if(N%4===0 && N%6===0){
+        console.log("Awesome!");
+    }else if(N%4===0){
+        console.log("Four!");
+    }else if(N%6===0){
+        console.log("Six!");
+    }else{
+        console.log("Dark!");
+    }
+}
+playOf4and6(12); // Awesome!
