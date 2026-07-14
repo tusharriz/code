e00badc8568ee0aa576a755e7d552969d3c2e275
Tushar Riz <er.tusharriz@gmail.com>
2026-07-14 22:15:11 +0530

    coding exercise

---
+++ JS/22-Function-I/08.1-perform-operations.js
@@ -0,0 +1,12 @@
+function performOperations(a,b){
+    console.log(a+b);
+    console.log(b-a);
+    console.log(a*b);
+    console.log(b/a);
+}
+performOperations(5,10);
+
+// 15
+// 5
+// 50
+// 2
