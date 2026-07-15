b3a8084405d037fdab85765623fbf2fff2802686
Tushar Riz <er.tusharriz@gmail.com>
2026-07-15 21:00:47 +0530

    coding exercise

---
+++ JS/22-Function-I/15-Write a function to check if the char is a small case or not.js
@@ -0,0 +1,74 @@
+// // Write a function to check if the char is a small case or not.
+
+function checkChar(char) {
+
+    let smallChar = "abcdefghijklmnopqrstuvwxyz";
+    let flag = false;
+
+    for (let i = 0; i <= smallChar.length - 1; i++) {
+        if (smallChar[i] === char) {
+            flag = true;
+            break;
+        }
+    }
+
+    if (flag == true) {
+        return "Yes," + " " + char + " " + "is a small case character."
+    } else {
+        return "No," + " " + char + " " + "is not a small case character."
+    }
+
+}
+
+console.log(checkChar("a")); // Yes, a is a small case character. 
+console.log(checkChar("A")); // No, A is not a small case character.
+
+
+let character = checkChar("b");
+console.log(character); // Yes, a is a small case.
+
+let character2 = checkChar("D");
+console.log(character2); // No, D is not a small case.
+
+
+{
+    // first do it without function
+    let char = "a"
+    let smallChar = "abcdefghijklmnopqrstuvwxyz";
+    let flag = false;
+
+    for (let i = 0; i <= smallChar.length - 1; i++) {
+        if (smallChar[i] == char) {
+            console.log(char + " " + "is a small case.");
+            flag == true;
+            break;
+        }
+    }
+    if (flag == false) {
+        console.log(char + " " + "is not a small case.")
+    }
+
+    // Now, do it again using function;
+    function checkChar(char) {
+        let smallChar = "abcdefghijklmnopqrstuvwxyz";
+        let flag = false;
+
+        for (let i = 0; i <= smallChar.length - 1; i++) {
+            if (smallChar[i] == char) {
+                flag == true;
+                return true;
+            }
+        }
+        if (flag == false) {
+            return false;
+        }
+    }
+
+    if(checkChar("e") == true){
+        console.log("char is a small case.");
+    }else{
+        console.log(" char is not a small case.");
+    }
+
+
+}
