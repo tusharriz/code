8b0f870a3d54cffc8cdad113b2b697a797a28b82
Tushar Riz <er.tusharriz@gmail.com>
2026-07-16 12:05:16 +0530

    coding exercise

---
+++ JS/23-Function-II/0.5-reverse-function.js
@@ -0,0 +1,13 @@
+// write a function to reverse a string;
+
+function reverseStr(str){
+    let nstr = "";
+
+    for(let i=str.length-1; i>=0; i--){
+        nstr = nstr + str[i];
+    }
+
+    return nstr;
+}
+
+console.log(reverseStr("Hello")); // olleH
