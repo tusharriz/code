7e772d0c27b1eb471b1b85d607a8069f130a6f3a
Tushar Riz <er.tusharriz@gmail.com>
2026-07-16 11:52:26 +0530

    coding exercise

---
+++ JS/23-Function-II/0.3-count-even-nums-btw-limit.js
@@ -0,0 +1,17 @@
+// count even numbers btw 1 to 100 using function 
+
+function checkEvenOdd(num){
+    if(num % 2 == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+let count = 0;
+
+for(let i=1; i<=100; i++){
+    if(checkEvenOdd(i) == true){
+        count++;
+    }
+}
+console.log(count);
