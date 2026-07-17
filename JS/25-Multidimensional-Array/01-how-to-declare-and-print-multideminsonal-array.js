a77156dd76eada340505644e4f839699497933d1
Tushar Riz <er.tusharriz@gmail.com>
2026-07-17 11:59:34 +0530

    coding exercise

---
+++ JS/25-Multidimensional-Array/01-how-to-declare-and-print-multideminsonal-array.js
@@ -0,0 +1,9 @@
+// how to declare and print multi dimensional array
+
+let arr = [ 1, 2, [ "A" , "B" ] ];
+
+console.log(arr[0]); // 1
+console.log(arr[1]); // 2
+console.log(arr[2]); // ["A", "B"]
+console.log(arr[2][0]); // ["A"]
+
