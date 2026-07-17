8721404676c3911936c93dcbb82f02c19c5662c7
Tushar Riz <er.tusharriz@gmail.com>
2026-07-17 19:34:54 +0530

    coding exercise

---
+++ JS/25-Multidimensional-Array/09-Given a matrix print it in the form of a snake.js
@@ -0,0 +1,52 @@
+// Given a matrix print it in the form of a snake
+
+let arr = [[1,2,3],[4,5,6],[7,8,9]];
+
+// 1  2  3
+// 4  5  6
+// 7  8  9
+
+for(let i=0; i<=arr.length-1; i++){
+    if(i%2==0){
+        for(let j=0; j<=arr.length-1; j++){
+            // console.log(arr[i][j]);
+        }
+    }else{
+        for(let j=arr.length-1; j>=0; j--){
+            // console.log(arr[i][j]);
+        }
+    }
+}
+
+// 1  2  3
+// 4  5  6
+// 7  8  9
+
+// 00 01 02
+// 10 11 12
+// 20 21 22
+{
+let arr = [[1,2,3],[4,5,6],[7,8,9]];
+for(let i=0; i<=arr.length-1; i++){
+    if(i==1){
+        for(let j=arr.length-1; j>=0; j--){
+            console.log(arr[i][j])
+        }
+    }
+    else{
+        for(let j=0; j<=arr.length-1; j++){
+        console.log(arr[i][j]);
+    }
+    }
+        
+}
+// 1
+// 2
+// 3
+// 6
+// 5
+// 4
+// 7
+// 8
+// 9
+}
