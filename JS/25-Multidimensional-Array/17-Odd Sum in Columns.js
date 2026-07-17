f8a755b189715e79115871ec314d96271eb464d0
Tushar Riz <er.tusharriz@gmail.com>
2026-07-17 23:19:12 +0530

    coding exercise

---
+++ JS/25-Multidimensional-Array/17-Odd Sum in Columns.js
@@ -0,0 +1,35 @@
+// Odd Sum in Columns
+
+function oddSumColumns(N,M,arr){
+    //write code here
+    
+    for(let i=0; i<=M-1; i++){
+        let sum = 0;
+        for(let j=0; j<=N-1; j++){
+            if(arr[j][i]%2!==0){
+                sum = sum + arr[j][i];
+            }
+        }
+        console.log(sum);
+    }
+}
+oddSumColumns(3,3,[[1,2,3],[4,5,6],[7,8,9]]);
+
+// 8
+// 5
+// 12
+
+let arr = [[1,2,3],[4,5,6],[7,8,9]];
+
+for(let i=0; i<=arr.length-1; i++){
+    let sum = 0;
+    for(let j=0; j<=arr.length-1; j++){
+        if(arr[j][i]%2!=0){
+            sum = sum + arr[j][i];
+        }
+    }
+    console.log(sum);
+}
+// 8
+// 5
+// 12
