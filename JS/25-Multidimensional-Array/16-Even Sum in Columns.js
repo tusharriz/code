5a270bb83511833b7a4c5cc90716ba3973098c97
Tushar Riz <er.tusharriz@gmail.com>
2026-07-17 23:16:36 +0530

    coding exercise

---
+++ JS/25-Multidimensional-Array/16-Even Sum in Columns.js
@@ -0,0 +1,35 @@
+// Even Sum in Columns
+
+function evenSumRows(N,M,arr){
+    //write code here
+    
+    for(let i=0; i<=N-1; i++){
+        let sum = 0;
+        for(let j=0; j<=M-1; j++){
+            if(arr[i][j]%2===0){
+                sum += arr[i][j];
+            }
+        }
+        // console.log(sum);
+    }
+}
+// evenSumRows(3,3,[[1,2,3],[4,5,6],[7,8,9]]);
+
+// 2
+// 10
+// 8
+
+let arr = [[1,2,3],[4,5,6],[7,8,9]];
+
+for(let i=0; i<=arr.length-1; i++){
+    let sum = 0;
+    for(let j=0; j<=arr.length-1; j++){
+        if(arr[j][i]%2==0){
+            sum = sum + arr[j][i];
+        }
+    }
+    console.log(sum);
+}
+// 4
+// 10
+// 6
