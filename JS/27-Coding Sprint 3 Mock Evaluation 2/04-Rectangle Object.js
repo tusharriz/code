6a5b3134f7b5eab22e9f82f6a2702c298bd6881d
Tushar Riz <er.tusharriz@gmail.com>
2026-07-18 13:29:55 +0530

    coding exercise

---
+++ JS/27-Coding Sprint 3 Mock Evaluation 2/04-Rectangle Object.js
@@ -0,0 +1,39 @@
+// Rectangle Object
+
+// - Create a rectangle object with length and width properties
+
+// - Create two methods area and perimeter, that will return the area and perimeter of the rectangle
+
+let rectangle = {
+    length : 5,
+    width : 9,
+    area : function(){
+        return this.length * this.width;
+    },
+
+    perimeter : function(){
+        return (this.length + this.width) * 2;
+    }
+};
+
+console.log(rectangle["area"]()); // 45
+console.log(rectangle["perimeter"]()); // 28
+
+{
+let rectangle = {
+    length : 5,
+    width : 9,
+    area : function(){
+        areaCal = this.length * this.width;
+        return areaCal;
+    },
+
+    perimeter : function(){
+        calPerimeter = 2 * (this.length + this.width);
+        return calPerimeter;
+    } 
+}
+
+console.log(rectangle["area"]()); // 300
+console.log(rectangle["perimeter"]()); // 70
+}
