27f853c38c33a92db76d6fe86e08a09d309afdd6
Tushar Riz <er.tusharriz@gmail.com>
2026-07-18 18:17:43 +0530

    coding exercise

---
+++ JS/28-Coding-C3-Evaluation/03-Maximum Occurring Element.js
@@ -0,0 +1,52 @@
+// Maximum Occurring Element
+
+function maximumOccuringElement(A,N){
+    //write code here
+    
+    let obj = {};
+    
+    for(let i=0; i<=N-1; i++){
+        if(obj[A[i]] === undefined){
+            obj[A[i]] = 1;
+        }else{
+            obj[A[i]]++;
+        }
+    }
+    // console.log(obj); // { '0': 2, '2': 1, '6': 1, '9': 1 }
+    
+    let max = -Infinity;
+    let index = 0;
+    
+    for(let key in obj){
+        if(obj[key] > max){
+            max = obj[key];
+            index =  key;
+            
+        }
+    }
+    // console.log(max); // 2
+    console.log(index); // 0
+}
+maximumOccuringElement([0,2,0,6,9],5); // 0
+
+let arr = [0,2,0,6,9];
+
+let obj = {};
+
+for(let i=0; i<=arr.length-1; i++){
+    if(obj[arr[i]] == undefined){
+        obj[arr[i]] = 1;
+    }else{
+        obj[arr[i]]++;
+    }
+}
+console.log(obj); // { '0': 2, '2': 1, '6': 1, '9': 1 }
+let index = 0;
+let max = -Infinity;
+for(let key in obj){
+    if(obj[key] > max){
+        max = obj[key];
+        index = Number(key);
+    }
+}
+console.log(index); // 0
