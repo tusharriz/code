59fc9012136070a7f2c57fc727cf96fdb9925ad2
Tushar Riz <er.tusharriz@gmail.com>
2026-07-18 20:03:09 +0530

    coding exercise

---
+++ JS/29-JS-Input-Taking/05-Strings - III.js
@@ -0,0 +1,29 @@
+function printStr(str) {
+  console.log(str);
+}
+printStr("aman"); // aman
+
+// function runProgram(input) {
+//   printStr(input);
+// }
+
+// if (process.env.USERNAME === "") {
+//   runProgram(``);
+// } else {
+//   process.stdin.resume();
+//   process.stdin.setEncoding("ascii");
+//   let read = "";
+//   process.stdin.on("data", function (input) {
+//     read += input;
+//   });
+//   process.stdin.on("end", function () {
+//     read = read.replace(/\n$/, "");
+//     read = read.replace(/\n$/, "");
+//     runProgram(read);
+//   });
+//   process.on("SIGINT", function () {
+//     read = read.replace(/\n$/, "");
+//     runProgram(read);
+//     process.exit(0);
+//   });
+// }
