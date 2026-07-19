09cb3b751ce712ee3bd31b92feafe05ba4dfed75
Tushar Riz <er.tusharriz@gmail.com>
2026-07-19 13:42:16 +0530

    coding exercise

---
+++ JS/19-Coding-Sprint-2-Mock-Evaluation-1/10-Check Palindrome.js
@@ -0,0 +1,18 @@
+// Check Palindrome
+
+function checkPalindrome(str) {
+    //write code here
+    let nstr = "";
+    
+    for(let i=str.length-1; i>=0; i--){
+        nstr = nstr + str[i];
+    }
+    
+    if(str === nstr){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
+  
+}
+checkPalindrome("nrupul"); // No
