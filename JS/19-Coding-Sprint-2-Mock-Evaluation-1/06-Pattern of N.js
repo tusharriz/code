628194862260978e18e463f175ac96ba52f44afa
Tushar Riz <er.tusharriz@gmail.com>
2026-07-19 13:41:11 +0530

    coding exercise

---
+++ JS/19-Coding-Sprint-2-Mock-Evaluation-1/06-Pattern of N.js
@@ -0,0 +1,18 @@
+// Pattern of N
+function patternOfN(N) {
+    // Write code here
+    let bag = "";
+    for(let i=1; i<=N*N; i++){
+        bag = bag + i + " ";
+        if(i%N===0){
+            bag = bag + "\n";
+        }
+    }
+    console.log(bag);
+}
+patternOfN(4);
+
+// 1 2 3 4
+// 5 6 7 8
+// 9 10 11 12
+// 13 14 15 16
