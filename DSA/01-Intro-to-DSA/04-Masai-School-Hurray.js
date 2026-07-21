a9f51b7ef7d6c3a5ea8664f36d6a42294b81ccb1
Tushar Riz <er.tusharriz@gmail.com>
2026-07-21 22:28:31 +0530

    coding exercise

---
+++ DSA/01-Intro-to-DSA/04-Masai-School-Hurray.js
@@ -0,0 +1,21 @@
+let n = 7;
+
+for(let i=1; i<=n; i++){
+    if(i%5==0 && i%7==0){
+        console.log("Masai School");
+    }else if(i%7==0){
+        console.log("Masai");
+    }else if(i%5==0){
+        console.log("School");
+    }else{
+        console.log("Hurray!")
+    }
+}
+
+// Hurray!
+// Hurray!
+// Hurray!
+// Hurray!
+// School
+// Hurray!
+// Masai
