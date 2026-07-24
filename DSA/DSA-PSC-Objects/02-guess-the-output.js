20e6c3143916a0781b1b4d036bab81db6d825fe7
Tushar Riz <er.tusharriz@gmail.com>
2026-07-24 18:58:48 +0530

    coding exercise

--- DSA/DSA-PSC-Objects/02-guess-the-output.js
+++ DSA/DSA-PSC-Objects/02-guess-the-output.js
@@ -6,3 +6,13 @@ let obj = {
 
 let key = "age";
 console.log(obj["key"]); // undefined
+
+let obj1 = {
+    name : "Doremon",
+    age : 17,
+    city: "Tokyo",
+    key : true
+}
+
+let key1 = "age";
+console.log(obj1["key"]); // true
