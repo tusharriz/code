bc04a3c3c00e04df345f372b48f37b3d3cae0689
Tushar Riz <er.tusharriz@gmail.com>
2026-07-26 23:13:04 +0530

    coding exercise

---
+++ DSA/DSA-111/03-DSA-Contest-3-Key-Value/02-Missing Vowels.js
@@ -0,0 +1,51 @@
+// Missing Vowels
+
+function missingVowel(N, str) {
+  //write code here
+  let obj = {
+    a: 0,
+    e: 0,
+    i: 0,
+    o: 0,
+    u: 0,
+  };
+
+  for (let i = 0; i <= N - 1; i++) {
+    if (obj[str[i]] === 0) {
+      obj[str[i]] = 1;
+    }
+  }
+  //   console.log(obj); // { a: 1, e: 1, i: 0, o: 0, u: 1 }
+
+  let bag = "";
+  for (let i in obj) {
+    if (obj[i] === 0) {
+      bag += i;
+    }
+  }
+
+  if (bag.length === 0) {
+    console.log(-1);
+  } else {
+    console.log(bag);
+  }
+}
+missingVowel(6, "nature"); // io
+
+let vowel = "aeiou";
+let str = "nature";
+
+let bag = "";
+for(let i=0; i<=vowel.length-1; i++){
+  let flag = false;
+  for(let j=0; j<=str.length-1; j++){
+    if(vowel[i]==str[j]){
+      flag = true;
+      break;
+    }
+  }
+  if(flag == false){
+    bag += vowel[i];
+  }
+}
+console.log(bag); // io
