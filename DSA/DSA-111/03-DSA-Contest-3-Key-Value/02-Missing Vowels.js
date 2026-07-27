580e879542aa462c749f2e799fbd66433d77047c
Tushar Riz <er.tusharriz@gmail.com>
2026-07-27 18:59:40 +0530

    coding exercise

--- DSA/DSA-111/03-DSA-Contest-3-Key-Value/02-Missing Vowels.js
+++ DSA/DSA-111/03-DSA-Contest-3-Key-Value/02-Missing Vowels.js
@@ -30,7 +30,7 @@ function missingVowel(N, str) {
     console.log(bag);
   }
 }
-missingVowel(6, "nature"); // io
+// missingVowel(6, "nature"); // io
 
 let vowel = "aeiou";
 let str = "nature";
@@ -48,4 +48,58 @@ for(let i=0; i<=vowel.length-1; i++){
     bag += vowel[i];
   }
 }
-console.log(bag); // io
+// console.log(bag); // io
+
+// OR
+
+let obj = {
+  a : 1,
+  e : 1,
+  i : 1,
+  o : 1,
+  u : 1
+}
+
+let s = "nature";
+// let s = "natureoi";
+
+for(let i=0; i<=s.length-1; i++){
+  if(obj[s[i]] == undefined){
+    continue;
+  }else{
+    obj[s[i]]++
+  }
+}
+// console.log(obj); // { a: 2, e: 2, i: 1, o: 1, u: 2 }
+
+// using for of loop
+for(let ele of s){
+  if(obj[ele] == undefined){
+    continue;
+  }else{
+    obj[ele]++;
+  }
+}
+// console.log(obj); // { a: 2, e: 2, i: 1, o: 1, u: 2 }
+
+// OR
+// another simple way to doing this is 
+
+for(let ele of s){
+  delete obj[ele]; //if ele is not present in obj it's simply continue without giving any error but if element is present in obj then it will delete the key
+}
+// console.log(obj); // { i: 1, o: 1 }
+
+
+let bag1 = "";
+for(let key in obj){
+  if(obj[key] == 1){
+    bag1 += key;
+  }
+}
+// console.log(bag1); // io
+if(bag1 == ""){
+  console.log(-1)
+}else{
+  console.log(bag1);
+}
