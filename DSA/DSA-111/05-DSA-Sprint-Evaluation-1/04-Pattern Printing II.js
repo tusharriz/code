0b4cef65965fe4683da482d1b7d6c5575b2f1c46
Tushar Riz <er.tusharriz@gmail.com>
2026-07-27 22:23:13 +0530

    coding exercise

---
+++ DSA/DSA-111/05-DSA-Sprint-Evaluation-1/04-Pattern Printing II.js
@@ -0,0 +1,53 @@
+// Pattern Printing II
+
+function patternPrintingII(N) {
+  // Write code here
+  for (let i = 1; i <= N; i++) {
+    let bag = "";
+    for (let j = 1; j <= N; j++) {
+      if (i == N && j == N) {
+        bag = bag + "*";
+      } else if (i == N) {
+        bag = bag + "*" + " ";
+      } else {
+        if (j == 1) {
+          bag = bag + "*" + " ";
+        } else if (j == N) {
+          bag = bag + "*";
+        } else {
+          bag = bag + " " + " ";
+        }
+      }
+    }
+    console.log(bag);
+  }
+}
+// patternPrintingII(5);
+
+let n=5;
+
+for(let i=1; i<=n; i++){
+  let bag = "";
+  for(let j=1; j<=n; j++){
+    if(i==n){
+      if(j==n){
+        bag = bag + "*";
+      }else{
+        bag = bag + "*" + " ";
+      }
+    }else if(j==1){
+      bag = bag + "*" + " "
+    }else if(j==n){
+        bag = bag + "*";
+    }else{
+      bag = bag + " " + " "
+    }
+  }
+  console.log(bag);
+}
+
+// *       *
+// *       *
+// *       *
+// *       *
+// * * * * *
