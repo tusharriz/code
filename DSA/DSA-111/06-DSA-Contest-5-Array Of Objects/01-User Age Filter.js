7756f74715dcea11741b9869c187cb1c196e553e
Tushar Riz <er.tusharriz@gmail.com>
2026-07-27 23:50:37 +0530

    coding exercise

---
+++ DSA/DSA-111/06-DSA-Contest-5-Array Of Objects/01-User Age Filter.js
@@ -0,0 +1,89 @@
+// User Age Filter
+
+// - Given a list of user data with name and age, create a list of key-value pairs from the input
+// - From the generated data, print the users whose age is more than 30
+
+// - Sample Input
+// ["Nrupul", "Prateek", "Aman", "Albert", "Yogesh"]
+// [32, 30, 26, 28, 44]
+
+// - Sample Data Stored `[]` - List `{}` - Key-Value Pairs
+
+// [
+// 	{name: "Nrupul", age: 32},
+// 	{name: "Prateek" , age: 30},
+// 	{name: "Aman" , age: 26},
+// 	{name: "Albert" , age: 28},
+// 	{name: "Yogesh" , age: 44},
+// ]
+
+// - Sample Output
+// `Nrupul Yogesh`
+
+let names = ["Nrupul", "Prateek", "Aman", "Albert", "Yogesh"];
+let age = [32, 30, 26, 28, 44];
+
+let arr = [];
+
+for(let i=0; i<=names.length-1; i++){
+    let obj = {};
+    obj["name"] = names[i];
+    obj["age"] = age[i];
+    // console.log(obj);
+    arr.push(obj);
+}
+// console.log(arr);
+
+// [
+//     { name: 'Nrupul', age: 32 },
+//     { name: 'Prateek', age: 30 },
+//     { name: 'Aman', age: 26 },
+//     { name: 'Albert', age: 28 },
+//     { name: 'Yogesh', age: 44 }
+// ]
+
+// - From the generated data, print the users whose age is more than 30
+
+let bag = "";
+
+for(let i=0; i<=arr.length-1; i++){
+    if(arr[i]["age"] > 30){
+        bag += arr[i]["name"] + " ";
+    }
+}
+
+// console.log(bag); // Nrupul Yogesh
+
+// OR
+
+{
+let names = ["Nrupul", "Prateek", "Aman", "Albert", "Yogesh"];
+let age = [32, 30, 26, 28, 44];
+
+let arr = [];
+
+for(let i=0; i<=names.length-1; i++){
+    let obj = {};
+    obj["name"] = names[i];
+    obj["age"] = age[i];
+    // console.log(obj);
+    arr.push(obj);
+
+}
+// console.log(arr);
+// [
+//   { name: 'Nrupul', age: 32 },
+//   { name: 'Prateek', age: 30 },
+//   { name: 'Aman', age: 26 },
+//   { name: 'Albert', age: 28 },
+//   { name: 'Yogesh', age: 44 }
+// ]
+let bag = "";
+for(let i=0; i<=arr.length-1; i++){
+    let obj = arr[i];
+    if(obj["age"] > 30){
+        bag += obj["name"] + " ";
+    }
+}
+console.log(bag); // Nrupul Yogesh
+}
