4b7903b4459c300550a41589d0e2fc7f4f6213d1
Tushar Riz <er.tusharriz@gmail.com>
2026-07-29 13:56:26 +0530

    coding exercise

---
+++ DSA/DSA-111/08-DSA-Contest-7-Object-Methods-1/05-findMax.js
@@ -0,0 +1,45 @@
+let obj = {
+    names : ["Ram", "Shayam", "Lakhan", "Aman"],
+    strength : [ 90,58,21,96],
+    iq : [75,15,38,78],
+    
+    findMaxStrength : function(){
+        
+        let arrStr = this.strength;
+        let maxStrength = -Infinity;
+        let name = "";
+
+
+        for(let i=0; i<=arrStr.length-1; i++){
+            if(arrStr[i] > maxStrength){
+                maxStrength = arrStr[i];
+                name = this.names[i]
+            }
+            
+        }
+        return name + " " +maxStrength;
+
+    },
+
+    findMaxIq : function(){
+
+        let arriq = this.iq;
+        let iq = -Infinity;
+        let name = "";
+
+
+        for(let i=0; i<=arriq.length-1; i++){
+            if(arriq[i] > iq){
+                iq = arriq[i];
+                name = this.names[i]
+            }
+            
+        }
+        return name + " " +iq;
+
+    }
+
+}
+
+console.log(obj["findMaxStrength"]());
+console.log(obj["findMaxIq"]());
