c09740d3601988b6f149bf78202b5d1bc06be603
Tushar Riz <er.tusharriz@gmail.com>
2026-07-30 11:09:50 +0530

    coding exercise

--- DSA/DSA-111/08-DSA-Contest-7-Object-Methods-1/05-findMax.js
+++ DSA/DSA-111/08-DSA-Contest-7-Object-Methods-1/05-findMax.js
@@ -1,44 +1,81 @@
+// let obj = {
+//     names : ["Ram", "Shayam", "Lakhan", "Aman"],
+//     strength : [ 90,58,21,96],
+//     iq : [75,15,38,78],
+
+//     findMaxStrength : function(){
+
+//         let arrStr = this.strength;
+//         let maxStrength = -Infinity;
+//         let name = "";
+
+
+//         for(let i=0; i<=arrStr.length-1; i++){
+//             if(arrStr[i] > maxStrength){
+//                 maxStrength = arrStr[i];
+//                 name = this.names[i]
+//             }
+
+//         }
+//         return name + " " +maxStrength;
+
+//     },
+
+//     findMaxIq : function(){
+
+//         let arriq = this.iq;
+//         let iq = -Infinity;
+//         let name = "";
+
+
+//         for(let i=0; i<=arriq.length-1; i++){
+//             if(arriq[i] > iq){
+//                 iq = arriq[i];
+//                 name = this.names[i]
+//             }
+
+//         }
+//         return name + " " +iq;
+
+//     }
+
+// }
+
+// console.log(obj["findMaxStrength"]());
+// console.log(obj["findMaxIq"]());
+
+// OR
+
 let obj = {
-    names : ["Ram", "Shayam", "Lakhan", "Aman"],
-    strength : [ 90,58,21,96],
-    iq : [75,15,38,78],
-    
-    findMaxStrength : function(){
-        
-        let arrStr = this.strength;
-        let maxStrength = -Infinity;
-        let name = "";
+    names: ["Ram", "Shayam", "Lakhan", "Aman"],
+    strength: [90, 58, 21, 96],
+    iq: [75, 15, 38, 78],
 
-
-        for(let i=0; i<=arrStr.length-1; i++){
-            if(arrStr[i] > maxStrength){
-                maxStrength = arrStr[i];
-                name = this.names[i]
+    findMaxStrength : function (){
+        let arr = this.strength;
+        let max = -Infinity;
+        let ans;
+        for(let i=0; i<=arr.length-1; i++){
+            if(arr[i] > max){
+                max = arr[i];
+                ans = this.names[i];
             }
-            
         }
-        return name + " " +maxStrength;
-
+        return ans;
     },
 
-    findMaxIq : function(){
-
-        let arriq = this.iq;
-        let iq = -Infinity;
-        let name = "";
-
-
-        for(let i=0; i<=arriq.length-1; i++){
-            if(arriq[i] > iq){
-                iq = arriq[i];
-                name = this.names[i]
+    findMaxIq : function (){
+        let arr = this.iq;
+        let max = -Infinity;
+        let ans;
+        for(let i=0; i<=arr.length-1; i++){
+            if(arr[i] > max){
+                max = arr[i];
+                ans = this.names[i]
             }
-            
         }
-        return name + " " +iq;
-
+        return ans;
     }
-
 }
 
 console.log(obj["findMaxStrength"]());
