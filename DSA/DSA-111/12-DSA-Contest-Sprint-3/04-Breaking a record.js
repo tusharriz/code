3f7bfcdf684a4d1074ca07e8bc6d9532ea97c687
Tushar Riz <er.tusharriz@gmail.com>
2026-08-07 10:16:52 +0530

    coding exercise

---
+++ DSA/DSA-111/12-DSA-Contest-Sprint-3/04-Breaking a record.js
@@ -0,0 +1,14 @@
+// Breaking a record
+
+function breakingTheRecord(n, m) {
+  //write code here
+
+  if (m > n) {
+    console.log("Broken");
+  } else if (m < n) {
+    console.log("Not Yet");
+  } else if (m === n) {
+    console.log("Wao");
+  }
+}
+breakingTheRecord(264, 200); // Not Yet
