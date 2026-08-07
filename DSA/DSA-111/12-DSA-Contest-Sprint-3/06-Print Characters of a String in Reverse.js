9de9f6025fc320d5977450fc6400455cff852cd4
Tushar Riz <er.tusharriz@gmail.com>
2026-08-07 10:17:50 +0530

    coding exercise

---
+++ DSA/DSA-111/12-DSA-Contest-Sprint-3/06-Print Characters of a String in Reverse.js
@@ -0,0 +1,18 @@
+// Print Characters of a String in Reverse
+
+function printCharReverse(N,str) {
+    //write code here
+    
+    for(let i=N-1; i>=0; i--){
+        console.log(str[i]);
+    }
+
+}
+printCharReverse(6,"ankush");
+
+// h
+// s
+// u
+// k
+// n
+// a
