bdf0446f6e7d0b01a2e623b38cd2b1676c8493b0
Tushar Riz <er.tusharriz@gmail.com>
2026-08-07 10:18:15 +0530

    coding exercise

---
+++ DSA/DSA-111/12-DSA-Contest-Sprint-3/07-Character At Even Position.js
@@ -0,0 +1,17 @@
+// Character At Even Position
+
+function charAtEvenPos(N,str) {
+    //write code here
+    
+    for(let i=0; i<=N-1; i++){
+        if(i%2===0){
+            console.log(str[i]);
+        }
+    }
+    
+}
+charAtEvenPos(6,"ankush"); 
+
+// a
+// k
+// s
