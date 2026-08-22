8fe6fa6f2ef0e84f58c4ac8f645703981ff50e25
Tushar Riz <er.tusharriz@gmail.com>
2026-08-22 22:46:27 +0530

    coding exercise

---
+++ JS/JS201/01-How-JS-Work/01-declaration-assignment-reassignment.js
@@ -0,0 +1,11 @@
+let userName; // this is called declaration or initialize a variable;
+// or in funny way "Baby is born, Will be named later"
+
+userName = "Nobita"; // this is called assignment, assign a value to a variable;
+// Baby born & named
+
+userName = "Doremon"; // this is called reassignment, basically assign a new value to variable;
+// Name changed later
+
+
+
